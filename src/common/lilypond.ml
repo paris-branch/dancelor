ecc0e9e5e584185f597c377c31d4619006d35e47
Niols <niols@niols.fr>
2019-01-23 22:17:54 +0100

    catch EPIPE around Lilypond

--- src/common/lilypond.ml
+++ src/common/lilypond.ml
@@ -16,23 +16,31 @@ let run ?(exec_path=".") ?(options=[]) filename =
 
   Log.debug (fun m -> m "Running Lilypond[%s]..." filename);
 
-  with_process_full
-    ~env:[|"PATH="^(Unix.getenv "PATH");
-           "LANG=en"|]
+  Lwt.catch
 
-    (shell
-       ("cd " ^ (escape_shell_argument exec_path) ^ " && "
-        ^ (shell_cmdline
-             ([Config.lilypond; "--loglevel=WARNING"] @ options @ [filename]))))
+    (fun () ->
+      with_process_full
+        ~env:[|"PATH="^(Unix.getenv "PATH");
+               "LANG=en"|]
 
-    (fun process ->
-      process#status >>= fun status ->
-      (match status with
-       | WEXITED 0 ->
-          Log.debug (fun m -> m "Lilypond[%s] exited with success." filename);
-          Lwt.return ()
-       | _ ->
-          Lwt_io.read process#stderr >>= fun output ->
-          Log.err (fun m -> m "Error while running Lilypond[%s]:@\n%a" filename pp_string_multiline output);
-          Lwt.return ())
-    )
+        (shell
+           ("cd " ^ (escape_shell_argument exec_path) ^ " && "
+            ^ (shell_cmdline
+                 ([Config.lilypond; "--loglevel=WARNING"] @ options @ [filename]))))
+
+        (fun process ->
+          process#status >>= fun status ->
+          (match status with
+           | WEXITED 0 ->
+              Log.debug (fun m -> m "Lilypond[%s] exited with success." filename);
+              Lwt.return ()
+           | _ ->
+              Lwt_io.read process#stderr >>= fun output ->
+              Log.err (fun m -> m "Error while running Lilypond[%s]:@\n%a" filename pp_string_multiline output);
+              Lwt.return ())))
+
+    (function
+     | Unix.Unix_error(Unix.EPIPE, _, _) ->
+        Log.debug (fun m -> m "Lilypond[%s] triggered an EPIPE error" filename);
+        Lwt.return ()
+     | exn -> Lwt.fail exn)
