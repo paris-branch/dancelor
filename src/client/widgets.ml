21e321005e3957b35606e8f4259da27a41ffd0b8
Victor Lanvin <victor.lanvin@irif.fr>
2019-02-08 00:43:45 +0100

    Better search bar

--- src/client/widgets.ml
+++ src/client/widgets.ml
@@ -5,66 +5,192 @@ module Html = Dom_html
 
 let js = Js.string
 
-let remove_children elt = 
-  while Js.to_bool elt##hasChildNodes do
-    let child = Js.Opt.get elt##.firstChild (fun () -> assert false) in
-    elt##removeChild child |> ignore
-  done
+module Utils = struct
 
-let add_classes elt classes =
-  List.iter (fun c -> elt##.classList##add (js c)) classes
+  let remove_children elt = 
+    while Js.to_bool elt##hasChildNodes do
+      let child = Js.Opt.get elt##.firstChild (fun () -> assert false) in
+      elt##removeChild child |> ignore
+    done
+  
+  let add_classes elt classes =
+    List.iter (fun c -> elt##.classList##add (js c)) classes
+  
+  let rem_classes elt classes = 
+    List.iter (fun c -> elt##.classList##remove (js c)) classes
+  
+  let set_parent elt parent = 
+    Dom.appendChild parent elt
 
-let rem_classes elt classes = 
-  List.iter (fun c -> elt##.classList##remove (js c)) classes
+  let is_focused document elt = 
+    document##.activeElement = Js.Opt.return (elt :> Html.element Js.t)
 
-let set_classes elt classes =
-  Option.ifsome (add_classes elt) classes
+end
 
-let form ?id ~parent ~document () =
-  let form = Html.createForm document in
-  Option.ifsome (fun i -> form##.id := js i) id;
-  Dom.appendChild parent form;
-  form
+module Elements = struct
 
-let hr ~parent ~document () =
-  Dom.appendChild parent (Html.createHr document)
+  let form ?id ?parent ~document () =
+    let form = Html.createForm document in
+    Option.ifsome (fun i -> form##.id := js i) id;
+    Option.ifsome (Utils.set_parent form) parent;
+    form
+  
+  let hr ?parent ~document () =
+    let hr = Html.createHr document in
+    Option.ifsome (Utils.set_parent hr) parent
+  
+  let br ?parent ~document () =
+    let br = Html.createBr document in
+    Option.ifsome (Utils.set_parent br) parent
+  
+  let div ?id ?classes ?parent ~document () = 
+    let div = Html.createDiv document in
+    Option.ifsome (fun i -> div##.id := js i) id;
+    Option.ifsome (Utils.add_classes div) classes;
+    Option.ifsome (Utils.set_parent div) parent;
+    div
+  
+  let table ?id ?classes ?parent ~document () = 
+    let table = Html.createTable document in
+    Option.ifsome (fun i -> table##.id := js i) id;
+    Option.ifsome (Utils.add_classes table) classes;
+    Option.ifsome (Utils.set_parent table) parent;
+    table
 
-let br ~parent ~document () =
-  Dom.appendChild parent (Html.createBr document)
+  let tr ?id ?classes ?parent ~document () = 
+    let tr = Html.createTr document in
+    Option.ifsome (fun i -> tr##.id := js i) id;
+    Option.ifsome (Utils.add_classes tr) classes;
+    Option.ifsome (Utils.set_parent tr) parent;
+    tr
 
-let div ?id ?classes ~parent ~document () = 
-  let div = Html.createDiv document in
-  Option.ifsome (fun i -> div##.id := js i) id;
-  set_classes div classes;
-  Dom.appendChild parent div;
-  div
+  let td ?id ?classes ?parent ~document () = 
+    let td = Html.createTd document in
+    Option.ifsome (fun i -> td##.id := js i) id;
+    Option.ifsome (Utils.add_classes td) classes;
+    Option.ifsome (Utils.set_parent td) parent;
+    td
 
-let text_input ?placeholder ?classes ?id ~parent ~document () =
-  let input = Html.createInput ~_type:(js "text") document in
-  Option.ifsome (fun p -> input##.placeholder := js p) placeholder;
-  Option.ifsome (fun i -> input##.id := js i) id;
-  set_classes input classes;
-  Dom.appendChild parent input;
-  input
+  let text_input ?placeholder ?classes ?id ?parent ~document () =
+    let input = Html.createInput ~_type:(js "text") document in
+    Option.ifsome (fun p -> input##.placeholder := js p) placeholder;
+    Option.ifsome (fun i -> input##.id := js i) id;
+    Option.ifsome (Utils.add_classes input) classes;
+    Option.ifsome (Utils.set_parent input) parent;
+    input
+  
+  let button ?classes ?text ?callback ?parent ~document () = 
+    let button = Html.createButton ~_type:(js "button") document in
+    Option.ifsome (Utils.add_classes button) classes;
+    Option.ifsome (fun t -> button##.textContent := Js.some (js t)) text;
+    Option.ifsome (fun callback ->
+      Lwt.async (fun () ->
+        Lwt_js_events.clicks button
+          (fun _ _ -> callback (); Lwt.return ()))) callback;
+    Option.ifsome (Utils.set_parent button) parent;
+    button
+  
+  let image ?classes ?src ?parent ~document () =
+    let html_image = Html.createImg document in
+    Option.ifsome (fun src -> html_image##.src := js src) src;
+    Option.ifsome (Utils.add_classes html_image) classes;
+    Option.ifsome (Utils.set_parent html_image) parent;
+    html_image
+  
+  let textnode ~text ?parent ~document () =
+    let text_node = document##createTextNode (js text) in
+    Option.ifsome (Utils.set_parent text_node) parent
 
-let button ?classes ?text ?callback ~parent ~document () = 
-  let button = Html.createButton ~_type:(js "button") document in
-  set_classes button classes;
-  Option.ifsome (fun t -> button##.textContent := Js.some (js t)) text;
-  Option.ifsome (fun callback ->
+end
+
+module SearchBar = struct
+
+  type t = {
+    document : Html.document Js.t;
+    main_div : Html.divElement Js.t;
+    search_bar : Html.inputElement Js.t;
+    search_results : Html.tableElement Js.t;
+    mutable has_results : bool;
+    mutable shown : bool;
+    mutable hovered : bool;
+  }
+
+  let hide t = 
+    if t.shown && (not t.hovered) 
+    && not (Utils.is_focused t.document t.search_bar) then begin
+      Dom.removeChild t.main_div t.search_results;
+      t.shown <- false
+    end
+
+  let show t = 
+    if (not t.shown) && t.has_results 
+    && Utils.is_focused t.document t.search_bar then begin
+      Dom.appendChild t.main_div t.search_results;
+      t.shown <- true
+    end
+
+  let create ?id ?placeholder ?parent ~document () =
+    let main_div = 
+      Elements.div ~classes:["search-area"] ?id ?parent ~document () 
+    in
+    let search_bar = 
+      Elements.text_input ~classes:["search-bar"] ~parent:main_div ?placeholder 
+        ~document () 
+    in
+    let search_results = 
+      Elements.table ~classes:["search-results"] ~document () 
+    in
+    let t = {
+      document;
+      main_div; 
+      search_bar; 
+      search_results; 
+      has_results = false;
+      shown = false;
+      hovered = false} 
+    in
     Lwt.async (fun () ->
-      Lwt_js_events.clicks button
-        (fun _ _ -> callback (); Lwt.return ()))) callback;
-  Dom.appendChild parent button;
-  button
+      Lwt_js_events.focuses t.search_bar
+        (fun _ _ -> show t; Lwt.return ()));
+    Lwt.async (fun () ->
+      Lwt_js_events.blurs t.search_bar
+        (fun _ _ -> hide t; Lwt.return ()));
+    Lwt.async (fun () ->
+      Lwt_js_events.mouseovers t.main_div
+        (fun _ _ -> t.hovered <- true; Lwt.return ()));
+    Lwt.async (fun () ->
+      Lwt_js_events.mouseouts t.main_div
+        (fun _ _ -> t.hovered <- false; hide t; Lwt.return ()));
+    t
 
-let image ?classes ?src ~parent ~document () =
-  let html_image = Html.createImg document in
-  Option.ifsome (fun src -> html_image##.src := js src) src;
-  set_classes html_image classes;
-  Dom.appendChild parent html_image;
-  html_image
+  let main_div t = t.main_div
 
-let textnode ~text ~parent ~document () =
-  let text_node = document##createTextNode (js text) in
-  Dom.appendChild parent text_node
+  let contents t = 
+    t.search_bar##.value |> Js.to_string
+  
+  let set_parent t p = 
+    Utils.set_parent t.main_div p
+
+  let add_result t ?callback elt =
+    t.has_results <- true;
+    show t;
+    Utils.set_parent elt t.search_results;
+    Utils.add_classes elt ["search-result"];
+    match callback with
+    | None -> ()
+    | Some cb ->
+      Lwt.async (fun () ->
+        Lwt_js_events.clicks elt
+          (fun _ev _ -> cb (); t.hovered <- false; hide t; Lwt.return ()))
+
+  let rem_results t = 
+    t.has_results <- false;
+    hide t;
+    Utils.remove_children t.search_results
+
+  let on_update t callback = 
+     Lwt.async (fun () ->
+      Lwt_js_events.keyups t.search_bar
+        (fun _ev _ -> callback (); Lwt.return ()))
+
+end
