c8b603618f21e8d8fd5b5985b14c7b086398374e
Niols <niols@niols.fr>
2020-05-20 15:02:09 +0200

    better debug of inkscape; detect version to adapt command line

--- lib/lilypond/lilypond.ml
+++ lib/lilypond/lilypond.ml
@@ -10,6 +10,88 @@ let shell_cmdline s =
   List.map escape_shell_argument s
   |> String.concat " "
 
+let lwt_process_pread2 ?timeout ?env ?cwd command =
+  Lwt_process.with_process_full ?timeout ?env ?cwd command @@ fun process ->
+  let%lwt status = process#status in
+  let%lwt stdout = Lwt_io.read process#stdout in
+  let%lwt stderr = Lwt_io.read process#stderr in
+  Lwt.return (status, stdout, stderr)
+
+module Inkscape = struct
+
+  (* FIXME: when versions <1 become obsolete (out of Debian stable), remove the
+     handling of different version numbers, and use the --export-overwrite
+     option. *)
+
+  let get_bin, is_version_pre1, set_bin =
+    let bin = ref "inkscape" in
+    let version_pre1 = ref None in
+    let get_bin () =
+      !bin
+    in
+    let is_version_pre1 () =
+      match !version_pre1 with
+      | None ->
+        Log.debug (fun m -> m "Polling Inkscape's version");
+        let%lwt (_, stdout, _) = lwt_process_pread2 ("", [|!bin; "--version"|]) in
+        let b =
+          if String.starts_with ~needle:"Inkscape 0." stdout then
+            (
+              Log.debug (fun m -> m "Inkscape's version is < 1");
+              true
+            )
+          else if String.starts_with ~needle:"Inkscape 1." stdout then
+            (
+              Log.debug (fun m -> m "Inkscape's version is >= 1");
+              false
+            )
+          else
+            failwith "Inkscape.is_version_pre1"
+        in
+        version_pre1 := Some b;
+        Lwt.return b
+      | Some b -> Lwt.return b
+    in
+    let set_bin nbin =
+      bin := nbin;
+      version_pre1 := None
+    in
+    (get_bin, is_version_pre1, set_bin)
+
+  let run ~exec_path cmd =
+    let cmdline = shell_cmdline cmd in
+    Log.debug (fun m -> m "Running %s" cmdline);
+    let cmd = Lwt_process.shell ("cd " ^ (escape_shell_argument exec_path) ^ " && " ^ cmdline) in
+    match%lwt lwt_process_pread2 cmd with
+    | (Unix.WEXITED 0, _, _) ->
+      Lwt.return ()
+
+    | (_, stdout, stderr) ->
+      Log.warn (fun m -> m "Inkscape failed!");
+      Log.warn (fun m -> m "Command line was:@\n%s" cmdline);
+
+      (* FIXME: log error status *)
+
+      if stdout = ""
+      then Log.warn (fun m -> m "Standard output is empty")
+      else Log.warn (fun m -> m "Standard output:@\n%s" stdout);
+
+      if stderr = ""
+      then Log.warn (fun m -> m "Standard error is empty")
+      else Log.warn (fun m -> m "Standard error:@\n%s" stderr);
+
+      Lwt.fail (Failure "Inkscape")
+
+  let crop ~exec_path ~output file =
+    let%lwt cmd =
+      if%lwt is_version_pre1 () then
+        Lwt.return [get_bin (); "--without-gui"; "--export-area-drawing"; "--export-plain-svg="^output; file]
+      else
+        Lwt.return [get_bin (); "--export-area-drawing"; "--export-plain-svg"; "--export-filename="^output; file]
+    in
+    run ~exec_path cmd
+end
+
 let run ?(lilypond_bin="lilypond") ?(exec_path=".") ?(options=[]) filename =
   let (>>=) = Lwt.bind in
   let open Lwt_process in
@@ -46,27 +128,9 @@ let run ?(lilypond_bin="lilypond") ?(exec_path=".") ?(options=[]) filename =
         Lwt.return ()
      | exn -> Lwt.fail exn)
 
-let cropped_svg ?lilypond_bin ?(inkscape_bin="inkscape") ?(exec_path=".") filename =
+let cropped_svg ?lilypond_bin ?(exec_path=".") filename =
   let%lwt () = run ?lilypond_bin ~exec_path ~options:["-dbackend=svg"] filename in
-  let%lwt status =
-    let cmd =
-      shell_cmdline
-        [inkscape_bin;
-         "-z"; "-D";
-         "-l"; (Filename.chop_extension filename) ^ ".cropped.svg";
-         (Filename.chop_extension filename) ^ ".svg"]
-    in
-    Log.debug (fun m -> m "Shell: %s" cmd);
-    Lwt_process.(
-      exec
-        ~stdout:`Dev_null
-        ~stderr:`Dev_null
-        (shell ("cd " ^ (escape_shell_argument exec_path) ^ " && " ^ cmd))
-    )
-  in
-  match status with
-  | Unix.WEXITED 0 ->
-    Lwt.return ()
-  | _ ->
-    Log.warn (fun m -> m "Inkscape failed!");
-    Lwt.fail (Failure "inkscape")
+  Inkscape.crop
+    ~exec_path
+    ((Filename.chop_extension filename) ^ ".svg")
+    ~output:((Filename.chop_extension filename) ^ ".cropped.svg")
