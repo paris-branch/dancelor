c7775f719cfeb13b192308a67a1388ab004aeece
Niols <niols@niols.fr>
2020-06-27 16:37:13 +0200

    --no-opam option

--- share/serve.sh
+++ share/serve.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
 set -uC ## no -e for this script
 
+usage () {
+    cat <<EOF
+Usage: $0 [OPTIONS]
+
+OPTIONS:
+
+  --no-pull   do not pull nor update OPAM
+  --no-opam   pull but do not update OPAM
+  --full      pull and update OPAM (default)
+
+  --help  print this help and exit
+EOF
+}
+
+rc=103
+
+while [ $# -gt 1 ]; do
+    case $1 in
+        --no-pull) rc=101 ;;
+        --no-opam) rc=102 ;;
+        --full)    rc=103 ;;
+
+        --help)
+            usage
+            exit 0
+            ;;
+    esac
+    shift
+done
+
 clean () { make clean; }
 pull () { git pull; }
 update_opam () { opam update; opam upgrade -y; }
