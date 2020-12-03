71cd5f0274cbd08e35196f85c76fa8f149b4b525
Niols <niols@niols.fr>
2020-12-03 17:23:42 +0100

    allow partial dates

--- lib/nes/nesDate.ml
+++ lib/nes/nesDate.ml
@@ -7,6 +7,12 @@ let is_none = (=) none
 
 let from_string s =
   match String.split_on_char '-' s |> List.map int_of_string_opt with
+  | [Some year] ->
+    (year, 0, 0)
+  | [Some year; Some month]
+    when month >= 1 && month <= 12
+    ->
+    (year, month, 0)
   | [Some year; Some month; Some day]
     when month >= 1 && month <= 12
          && day >= 1 && day <= 31
@@ -16,7 +22,14 @@ let from_string s =
     failwith "NesDate.from_string"
 
 let to_string (year, month, day) =
-  spf "%04d-%02d-%02d" year month day
+  if year = 0 then
+    ""
+  else if month = 0 then
+    spf "%04d" year
+  else if day = 0 then
+    spf "%04d-%02d" year month
+  else
+    spf "%04d-%02d-%02d" year month day
 
 let to_yojson date =
   `String (to_string date)
