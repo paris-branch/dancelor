d88ba5a90a9bedeb6bcdf264b1a5e0e046688b89
Niols <niols@niols.fr>
2021-07-04 00:18:02 +0200

    trad slug

--- src/common/model/credit.ml
+++ src/common/model/credit.ml
@@ -14,8 +14,8 @@ let status c = Lwt.return c.status
 let line c = Lwt.return c.line
 let persons c = Lwt.return c.persons
 
-let trad = Slug.unsafe_of_string "traditional"
-let is_trad c = Slug.equal c.slug trad
+let trad_slug = Slug.unsafe_of_string "traditional"
+let is_trad c = Slug.equal c.slug trad_slug
 
 let equal credit1 credit2 =
   let%lwt slug1 = slug credit1 in
