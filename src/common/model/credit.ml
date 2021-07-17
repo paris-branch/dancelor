69fcc7942bdd93df8f39da67a555f2050ad9618d
Niols <niols@niols.fr>
2021-07-17 18:33:07 +0200

    scddb id in credits

--- src/common/model/credit.ml
+++ src/common/model/credit.ml
@@ -6,13 +6,15 @@ type t =
   { slug : t Slug.t ;
     status : Status.t [@default Status.bot] ;
     line : string ;
-    persons : Person.t Slug.t list [@default []] }
+    persons : Person.t Slug.t list [@default []];
+    scddb_id : int option [@default None] [@key "scddb-id"] }
 [@@deriving yojson, make]
 
 let slug c = Lwt.return c.slug
 let status c = Lwt.return c.status
 let line c = Lwt.return c.line
 let persons c = Lwt.return c.persons
+let scddb_id c = Lwt.return c.scddb_id
 
 let trad_slug = Slug.unsafe_of_string "traditional"
 let is_trad c = Slug.equal c.slug trad_slug
