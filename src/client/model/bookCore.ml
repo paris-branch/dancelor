0ab60276bd456d3ab36aa094f36badf0d40447f6
Aymeric Fromherz <aymeric.fr@hotmail.fr>
2021-12-14 16:01:05 +0100

    Start refactoring duplicateVersion warning creation in bookViewer to log which sets tunes belong to

--- src/client/model/bookCore.ml
+++ src/client/model/bookCore.ml
@@ -47,11 +47,43 @@ let warnings p =
         | _ -> Lwt.return_none)
       contents
   in
-  let%lwt contained_versions =
-    let%lwt versions_and_parameters = Lwt_list.map_s Set.versions_and_parameters sets in
-    let versions_and_parameters = List.flatten versions_and_parameters in
-    Lwt.return (List.map fst versions_and_parameters)
+
+  (* Extend the list of sets associated to this tune. Creates it if it was not yet in the hashtable *)
+  let extend htbl tune set_opt =
+    match Hashtbl.find_opt htbl tune with
+    | None -> Hashtbl.add htbl tune [set_opt]
+    | Some sets -> Hashtbl.replace htbl tune (set_opt::sets)
   in
+  
+  (* [tunes_to_set] is a hashtable from tunes to sets they belong to.
+     Standalone tunes are associated with None *)
+  let tunes_to_set = Hashtbl.create 8 in
+  let%lwt _ = Lwt_list.iter_s
+    (fun v -> 
+      let%lwt tune = Version.tune v in
+      extend tunes_to_set tune None;
+      Lwt.return ())
+    standalone_versions in
+  
+  let%lwt _ = Lwt_list.iter_s (fun set ->
+      let%lwt versions_and_parameters = Set.versions_and_parameters set in
+      let versions = List.map fst versions_and_parameters in
+      let%lwt _ = Lwt_list.iter_s
+        (fun v ->
+           let%lwt tune = Version.tune v in
+           extend tunes_to_set tune (Some set);
+           Lwt.return ()
+        ) versions in
+      Lwt.return ()
+    ) sets in
+
+  Hashtbl.iter
+    (fun tune sets_opt -> if List.length sets_opt > 1 then
+         add_warning (DuplicateVersion tune)
+    )
+    tunes_to_set; 
+
+(*
   let versions = standalone_versions @ contained_versions in
   let%lwt tunes = Lwt_list.map_s Version.tune versions in
   let tunes = List.sort Stdlib.compare tunes in
@@ -68,6 +100,7 @@ let warnings p =
          tunes
      in
      ());
+*)
 
   (* Return *)
   Lwt.return !warnings
