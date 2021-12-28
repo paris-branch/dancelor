3eba2ec0310baea4a8031774848393d503a69ea1
Niols <niols@dancelor.org>
2021-12-28 15:50:29 +0100

    add documentation

--- lib/nes/nesInt.ml
+++ lib/nes/nesInt.ml
@@ -3,11 +3,5 @@ include Int
 let to_english_string n =
   NesInt64.to_english_string (Int64.of_int n)
 
-let to_english_string_unsigned n =
-  NesInt64.to_english_string_unsigned (Int64.of_int n)
-
 let to_english_string_times n =
   NesInt64.to_english_string_times (Int64.of_int n)
-
-let to_english_string_times_unsigned n =
-  NesInt64.to_english_string_times_unsigned (Int64.of_int n)
---
+++ lib/nes/nesInt.mli
@@ -0,0 +1,22 @@
+(** {1 Integers} *)
+
+(** {2 Standard Library}
+
+   This module contains everything defined for integers by the OCaml standard
+   library. For these functions, refer to the official documentation. *)
+
+include module type of Stdlib.Int
+
+(** {2 Additional Contents} *)
+
+(** {3 Printing Numbers as English Words}
+
+    For compatibility with all platforms, including 32-bit platforms or JS
+   extraction, these functions are defined in the 64-bit integer modules
+   {!NesInt64}. Refer to this module for the documentation. *)
+
+val to_english_string : int -> string
+(** See {!NesInt64.to_english_string}. *)
+
+val to_english_string_times : int -> string
+(** See {!NesInt64.to_english_string_times}. *)
