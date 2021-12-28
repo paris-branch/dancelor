d6f8d779427cffdf1a1bedc9981789e9453512f0
Niols <niols@dancelor.org>
2021-12-28 15:31:23 +0100

    rewrite using lists; handle min_int & all unsigned

--- lib/nes/nesInt64.ml
+++ lib/nes/nesInt64.ml
@@ -1,98 +1,99 @@
 include Int64
 
-let nums =
+let nums_to_english_string =
   let nums = [| (* 0 -> 19  *)
-    ""; " one"; " two"; " three"; " four";
-    " five"; " six"; " seven"; " eight"; " nine";
-    " ten"; " eleven"; " twelve"; " thirteen"; " fourteen";
-    " fifteen"; " sixteen"; " seventeen"; " eighteen"; " nineteen"
+    []; ["one"]; ["two"]; ["three"]; ["four"];
+    ["five"]; ["six"]; ["seven"]; ["eight"]; ["nine"];
+    ["ten"]; ["eleven"]; ["twelve"]; ["thirteen"]; ["fourteen"];
+    ["fifteen"]; ["sixteen"]; ["seventeen"]; ["eighteen"]; ["nineteen"]
   |] in
   fun n -> nums.(to_int n)
 
-let tens =
+let tens_to_english_string =
   let tens = [| (* 0, 10 -> 90 *)
-    ""; " ten"; " twenty"; " thirty"; " forty";
-    " fifty"; " sixty"; " seventy"; " eighty"; " ninety"
+    []; ["ten"]; ["twenty"]; ["thirty"]; ["forty"];
+    ["fifty"]; ["sixty"]; ["seventy"]; ["eighty"]; ["ninety"]
   |] in
   fun n -> tens.(to_int n)
 
-let to_english_string =
+let ones_to_english_string =
   (* first a version only for numbers between 0 and 1000 *)
   fun number ->
     assert (zero <= number && number < 1_000L);
-    let so_far = ref "" in
+    let so_far = ref [] in
     let number = ref number in
     if rem !number 100L < 20L then
       (
-        so_far := nums (rem !number 100L);
+        so_far := nums_to_english_string (rem !number 100L);
         number := div !number 100L
       )
     else
       (
-        so_far := nums (rem !number 10L);
+        so_far := nums_to_english_string (rem !number 10L);
         number := div !number 10L;
-        so_far := tens (rem !number 10L) ^ !so_far;
+        so_far := tens_to_english_string (rem !number 10L) @ !so_far;
         number := div !number 10L
       );
     if !number = 0L then !so_far
-    else nums !number ^ " hundred" ^ !so_far
+    else nums_to_english_string !number @ ["hundred"] @ !so_far
 
-let to_english_string number =
-  (* generic version for positive numbers *)
-  (* FIXME: would not work on int32 systems; so maybe this function should be
-     defined in the int64 module and used from here with a conversion *)
-  assert (0L <= number);
+let to_english_string_unsigned number =
+  (* the following code works up to quintillions, that is for any number
+     strictly below 1000 * 10^18, which is enough for 2^64 *)
+  let rem = unsigned_rem in
+  let div = unsigned_div in
   if number = 0L then
-    "zero"
+    ["zero"]
   else
     (
-      let result =
-        (let quintillions = rem (div number 1_000_000_000_000_000_000L) 1_000L in
-         match quintillions with
-         | 0L -> ""
-         | 1L -> "one quintillion "
-         | _ -> to_english_string quintillions ^ " quintillion ")
-        ^
-        (let quadrillions = rem (div number 1_000_000_000_000_000L) 1_000L in
-         match quadrillions with
-         | 0L -> ""
-         | 1L -> "one quadrillion "
-         | _ -> to_english_string quadrillions ^ " quadrillion ")
-        ^
-        (let trillions = rem (div number 1_000_000_000_000L) 1_000L in
-         match trillions with
-         | 0L -> ""
-         | 1L -> "one trillion "
-         | _ -> to_english_string trillions ^ " trillion ")
-        ^
-        (let billions = rem (div number 1_000_000_000L) 1_000L in
-         match billions with
-         | 0L -> ""
-         | 1L -> "one billion "
-         | _ -> to_english_string billions ^ " billion ")
-        ^
-        (let millions  = rem (div number 1_000_000L) 1_000L in
-         match millions with
-         | 0L -> ""
-         | 1L -> "one million "
-         | _ -> to_english_string millions ^ " million ")
-        ^
-        (let thousands = rem (div number 1_000L) 1_000L in
-         match thousands with
-         | 0L -> ""
-         | 1L -> "one thousand "
-         | _ -> to_english_string thousands ^ " thousand ")
-        ^
-        (let ones = rem number 1_000L in
-         to_english_string ones)
-      in
-      NesString.remove_duplicates ~char:' ' result
+      (let quintillions = rem (div number 1_000_000_000_000_000_000L) 1_000L in
+       match quintillions with
+       | 0L -> []
+       | 1L -> ["one"; "quintillion"]
+       | _ -> ones_to_english_string quintillions @ ["quintillion"])
+      @
+      (let quadrillions = rem (div number 1_000_000_000_000_000L) 1_000L in
+       match quadrillions with
+       | 0L -> []
+       | 1L -> ["one"; "quadrillion"]
+       | _ -> ones_to_english_string quadrillions @ ["quadrillion"])
+      @
+      (let trillions = rem (div number 1_000_000_000_000L) 1_000L in
+       match trillions with
+       | 0L -> []
+       | 1L -> ["one"; "trillion"]
+       | _ -> ones_to_english_string trillions @ ["trillion"])
+      @
+      (let billions = rem (div number 1_000_000_000L) 1_000L in
+       match billions with
+       | 0L -> []
+       | 1L -> ["one"; "billion"]
+       | _ -> ones_to_english_string billions @ ["billion"])
+      @
+      (let millions  = rem (div number 1_000_000L) 1_000L in
+       match millions with
+       | 0L -> []
+       | 1L -> ["one"; "million"]
+       | _ -> ones_to_english_string millions @ ["million"])
+      @
+      (let thousands = rem (div number 1_000L) 1_000L in
+       match thousands with
+       | 0L -> []
+       | 1L -> ["one"; "thousand"]
+       | _ -> ones_to_english_string thousands @ ["thousand"])
+      @
+      (let ones = rem number 1_000L in
+       ones_to_english_string ones)
     )
 
+let to_english_string_unsigned number =
+  String.concat " " (to_english_string_unsigned number)
+
 let to_english_string number =
-  assert (number <> min_int);
+  (* note: abs min_int = min_int but it is okay because min_int = max_int + 1 so
+     the unsigned function will work just fine *)
   (if number >= 0L then "" else "minus ")
-  ^ to_english_string (abs number)
+  ^ to_english_string_unsigned (abs number)
 
 let%test _ = to_english_string 0L = "zero"
 let%test _ = to_english_string 1L = "one"
@@ -111,12 +112,23 @@ let%test _ = to_english_string 700_000L = "seven hundred thousand"
 let%test _ = to_english_string 9_000_000L = "nine million"
 let%test _ = to_english_string 9_001_000L = "nine million one thousand"
 let%test _ = to_english_string 123_456_789L =
-             "one hundred twenty three million "
-             ^ "four hundred fifty six thousand seven hundred eighty nine"
+             "one hundred twenty three million"
+             ^ " four hundred fifty six thousand"
+             ^ " seven hundred eighty nine"
 let%test _ = to_english_string 2_147_483_647L =
-             "two billion one hundred forty seven million "
-             ^ "four hundred eighty three thousand six hundred forty seven"
+             "two billion"
+             ^ " one hundred forty seven million"
+             ^ " four hundred eighty three thousand"
+             ^ " six hundred forty seven"
 let%test _ = to_english_string 3_000_000_010L = "three billion ten"
+let%test _ = to_english_string (-9_223_372_036_854_775_808L) =
+             "minus nine quintillion"
+             ^ " two hundred twenty three quadrillion"
+             ^ " three hundred seventy two trillion"
+             ^ " thirty six billion"
+             ^ " eight hundred fifty four million"
+             ^ " seven hundred seventy five thousand"
+             ^ " eight hundred eight"
 
 let to_english_times_string = function
   | 1L -> "once"
