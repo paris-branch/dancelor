9cf6e16efb7736889f100b17e7fedb2ed4756643
Aymeric Fromherz <aymeric.fr@hotmail.fr>
2022-01-14 23:53:31 +0100

    List all broken versions on version/broken

--- src/client/versionBrokenExplorer.ml
+++ src/client/versionBrokenExplorer.ml
@@ -1,5 +1,7 @@
+open Nes
 open Js_of_ocaml
 open Dancelor_client_elements
+open Dancelor_client_model
 
 let js = Js.string
 
@@ -13,10 +15,20 @@ let create page =
   let document = Page.document page in
   let content = Dom_html.createDiv document in
 
-  document##.title := js ("List of Broken Versions");
+  document##.title := js ("Broken Versions");
 
   Dancelor_client_html.(append_nodes (content :> dom_node) (Page.document page) [
-    h2 ~classes:["title"] [text "Hello"]
+    h2 ~classes:["title"] [text "List of Broken Versions"];
+
+    div_lwt (
+      let%lwt versions = Version.search VersionFilter.broken >|=| Score.list_erase in
+
+      (* If there is no broken version, no need to print the table *)
+      if List.length versions = 0 then
+        Lwt.return [ text "No broken version" ]
+      else
+        Lwt.return [ Dancelor_client_tables.versions_with_names versions ]
+    )
   ]);
 
   {page; content}
