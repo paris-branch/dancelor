97d6dd10618cd225c52bbc7467dc5ec84da6075e
Niols <niols@dancelor.org>
2022-01-18 22:53:37 +0100

    full rewrite of the warnings function
    - one function per type of warnings
    - helpers for common tasks (eg. getting only sets)
    - limit side effects (return lists)

--- src/client/model/bookCore.ml
+++ src/client/model/bookCore.ml
@@ -11,109 +11,139 @@ let contents p =
       | InlineSet (s, p) -> Lwt.return (InlineSet (s, p)))
     contents
 
-let warnings p =
-  let warnings = ref [] in
-  let add_warning w = warnings := w :: !warnings in
+module Warnings = struct
+  let empty dance =
+    let%lwt contents = contents dance in
+    if contents = [] then
+      Lwt.return [Empty]
+    else
+      Lwt.return_nil
 
-  let%lwt contents = contents p in
-
-  (* Raise the [Empty] warning if there is nothing in this book *)
-  if contents = [] then add_warning Empty;
-
-  (* Check that there are no duplicate sets. *)
-  let%lwt sets =
-    Lwt_list.filter_map_p
-      (function
-        | Version _ -> Lwt.return_none
-        | Set (s, _) | InlineSet (s, _) -> Lwt.return_some s)
-      contents
-  in
-  let%lwt sets = List.sort_lwt Set.compare sets in
-  (match sets with
-   | [] -> ()
-   | set :: sets ->
-     let _ =
-       List.fold_left
-         (fun prev curr ->
-            if prev = curr then
-              add_warning (DuplicateSet curr);
-            curr)
-         set sets
-     in
-     ());
-
-  (* remove duplicate sets to avoid further warnings *)
-  (* FIXME: we know that [sets] is sorted so we could use something more
-     efficient here *)
-  let%lwt sets = List.sort_uniq_lwt Set.compare sets in
-
-  (* Check that there are no duplicate tune. *)
-  let%lwt standalone_versions =
+  let standalone_versions dance =
+    let%lwt contents = contents dance in
     Lwt_list.filter_map_p
       (function
         | Version (v, _) -> Lwt.return_some v
         | _ -> Lwt.return_none)
       contents
-  in
 
-  (* Extend the list of sets associated to this tune. Creates it if it was not yet in the hashtable *)
-  let extend htbl tune set_opt =
-    match Hashtbl.find_opt htbl tune with
-    | None -> Hashtbl.add htbl tune [set_opt]
-    | Some sets -> Hashtbl.replace htbl tune (set_opt :: sets)
-  in
+  let sets dance =
+    let%lwt contents = contents dance in
+    Lwt_list.filter_map_p
+      (function
+        | Version _ -> Lwt.return_none
+        | Set (s, _) | InlineSet (s, _) -> Lwt.return_some s)
+      contents
 
-  (* [tunes_to_set] is a hashtable from tunes to sets they belong to.
-     Standalone tunes are associated with None *)
-  let tunes_to_set = Hashtbl.create 8 in
-  (* register standalone tunes *)
-  Lwt_list.iter_s
-    (fun v ->
-       let%lwt tune = Version.tune v in
-       extend tunes_to_set tune None;
-       Lwt.return ())
-    standalone_versions;%lwt
-  (* register tunes in sets *)
-  Lwt_list.iter_s
-    (fun set ->
-       let%lwt versions_and_parameters = Set.versions_and_parameters set in
-       let versions = List.map fst versions_and_parameters in
-       Lwt_list.iter_s
-         (fun v ->
-            let%lwt tune = Version.tune v in
-            extend tunes_to_set tune (Some set);
-            Lwt.return ())
-         versions)
-    sets;%lwt
-  (* crawl all registered tunes and see if they appear several times. if that is
-     the case, add a warning accordingly *)
-  Hashtbl.to_seq tunes_to_set
-  |> Seq.iter_lwt
-    (fun (tune, sets_opt) ->
-       let%lwt sets_opt = List.sort_count_lwt (Option.compare_lwt Set.compare) sets_opt in
-       if List.length sets_opt > 1 then
-          add_warning (DuplicateVersion (tune, sets_opt));
-       Lwt.return_unit);%lwt
+  let unique_sets dance =
+    let%lwt sets = sets dance in
+    List.sort_uniq_lwt Set.compare sets
 
-  (* Check if a set points to a dance with a different kind *)
-  Lwt_list.iter_s
-    (function
-      | Set (set, p) | InlineSet (set, p) ->
-        (
-          match SetParameters.for_dance p with
-          | None -> Lwt.return_unit
-          | Some dance_slug ->
-            let%lwt dance = Dance.get dance_slug in
-            let%lwt dance_kind = DanceCore.kind dance in
-            let%lwt set_kind = SetCore.kind set in
-            if set_kind = dance_kind then Lwt.return_unit
-            else (add_warning (SetDanceMismatch (set, dance)); Lwt.return_unit)
-        )
-      | Version _ -> Lwt.return_unit)
-    contents;%lwt
+  let sets_and_parameters dance =
+    let%lwt contents = contents dance in
+    Lwt_list.filter_map_p
+      (function
+        | Set (set, parameters) | InlineSet (set, parameters) ->
+          Lwt.return_some (set, parameters)
+        | Version _ -> Lwt.return_none)
+      contents
 
-  (* Return *)
-  Lwt.return !warnings
+  let duplicateSet dance =
+    let%lwt sets = sets dance in
+    match%lwt List.sort_lwt Set.compare sets with
+    | [] -> Lwt.return_nil
+    | first_set :: other_sets ->
+      let%lwt (_, warnings) =
+        Lwt_list.fold_left_s
+          (fun (previous_set, warnings) current_set ->
+             let%lwt warnings =
+               if%lwt Set.equal current_set previous_set then
+                 Lwt.return ((DuplicateSet current_set) :: warnings)
+               else
+                 Lwt.return warnings
+             in
+             Lwt.return (current_set, warnings))
+          (first_set, [])
+          other_sets
+      in
+      Lwt.return warnings
+
+  let duplicateVersion dance =
+    let%lwt sets = unique_sets dance in
+    let%lwt standalone_versions = standalone_versions dance in
+    (* [tunes_to_sets] is a hashtable from tunes to sets they belong to.
+       Standalone tunes are associated with None *)
+    let tunes_to_sets = Hashtbl.create 8 in
+    (* Extend the list of sets associated to this tune. Creates it if it was not
+       yet in the hashtable *)
+    let register_tune_to_set tune set_opt =
+      match Hashtbl.find_opt tunes_to_sets tune with
+      | None -> Hashtbl.add tunes_to_sets tune [set_opt]
+      | Some set_opts -> Hashtbl.replace tunes_to_sets tune (set_opt :: set_opts)
+    in
+    (* register standalone tunes *)
+    Lwt_list.iter_s
+      (fun v ->
+         let%lwt tune = Version.tune v in
+         register_tune_to_set tune None;
+         Lwt.return ())
+      standalone_versions;%lwt
+    (* register tunes in sets *)
+    Lwt_list.iter_s
+      (fun set ->
+         let%lwt versions_and_parameters = Set.versions_and_parameters set in
+         let versions = List.map fst versions_and_parameters in
+         Lwt_list.iter_s
+           (fun v ->
+              let%lwt tune = Version.tune v in
+              register_tune_to_set tune (Some set);
+              Lwt.return ())
+           versions)
+      sets;%lwt
+    (* crawl all registered tunes and see if they appear several times. if that is
+       the case, add a warning accordingly *)
+    Hashtbl.to_seq tunes_to_sets
+    |> List.of_seq
+    |> Lwt_list.fold_left_s
+      (fun warnings (tune, set_opts) ->
+         let%lwt set_opts = List.sort_count_lwt (Option.compare_lwt Set.compare) set_opts in
+         if List.length set_opts > 1 then
+           Lwt.return ((DuplicateVersion (tune, set_opts)) :: warnings)
+         else
+           Lwt.return warnings)
+      []
+
+  let setDanceMismatch dance =
+    let%lwt sets_and_parameters = sets_and_parameters dance in
+    Lwt_list.filter_map_p
+      (fun (set, parameters) ->
+         match SetParameters.for_dance parameters with
+         | None -> Lwt.return_none
+         | Some dance_slug ->
+           (* FIXME: SetParameters should be hidden behind the same kind of
+              mechanism as the rest; and this step should not be necessary *)
+           let%lwt dance = Dance.get dance_slug in
+           let%lwt dance_kind = DanceCore.kind dance in
+           let%lwt set_kind = SetCore.kind set in
+           if set_kind = dance_kind then
+             Lwt.return_none
+           else
+             Lwt.return_some (SetDanceMismatch (set, dance)))
+      sets_and_parameters
+
+  let all dance =
+    Lwt_list.fold_left_s
+      (fun warnings new_warnings_lwt ->
+         let%lwt new_warnings = new_warnings_lwt in
+         Lwt.return (warnings @ new_warnings))
+      []
+      [ empty dance;
+        duplicateSet dance;
+        duplicateVersion dance;
+        setDanceMismatch dance ]
+end
+
+let warnings dance = Warnings.all dance
 
 let get the_slug =
   let open BookEndpoints in
