c448f4068aaee4815ae1fd3f5e6cbab1f80843ad
Aymeric Fromherz <aymeric.fr@hotmail.fr>
2022-01-21 11:38:16 +0100

    Add query_parameters handling of version get controller

--- src/server/controller/version.ml
+++ src/server/controller/version.ml
@@ -1,4 +1,5 @@
 open Nes
+open Dancelor_common
 open Dancelor_server_model
 module Log = (val Dancelor_server_logs.create "controller.version" : Logs.LOG)
 
@@ -7,8 +8,10 @@ let get_ly version _ =
   let%lwt body = Version.content version in
   Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body ()
 
-let prepare_ly_file ?(show_meta=false) ?(meta_in_title=false) ~fname version =
+let prepare_ly_file ?(parameters=VersionParameters.none) ?(show_meta=false) ?(meta_in_title=false) ~fname version =
   Log.debug (fun m -> m "Preparing Lilypond file");
+  let parameters = VersionParameters.fill parameters in
+
   let fname_scm = Filename.chop_extension fname ^ ".scm" in
   let%lwt tune = Version.tune version in
   let%lwt name = Tune.name tune in
@@ -33,6 +36,14 @@ let prepare_ly_file ?(show_meta=false) ?(meta_in_title=false) ~fname version =
   let (tempo_unit, tempo_value) = Kind.base_tempo kind in
   Log.debug (fun m -> m "Getting content");
   let%lwt content = Version.content version in
+  let content =
+    match parameters |> VersionParameters.clef with
+    | None -> content
+    | Some clef_parameter ->
+      let clef_regex = Str.regexp "\\\\clef *\"?[a-z]*\"?" in
+      Str.global_replace clef_regex ("\\clef " ^ Music.clef_to_lilypond_string clef_parameter) content
+  in
+
   Log.debug (fun m -> m "Generating Scheme & LilyPond string");
   let lilypond =
     Format.with_formatter_to_string @@ fun fmt ->
@@ -47,6 +58,7 @@ let prepare_ly_file ?(show_meta=false) ?(meta_in_title=false) ~fname version =
     fpf fmt [%blob "template/scottish-chords.ly"];
     fpf fmt [%blob "template/fancy-unfold-repeats.ly"];
     fpf fmt [%blob "template/header.ly"] title subtitle;
+
     fpf fmt [%blob "template/version/header.ly"];
     fpf fmt [%blob "template/version.ly"] piece opus content tempo_unit tempo_value (Kind.base_to_pretty_string ~capitalised:false kind) content
   in
@@ -76,7 +88,7 @@ let prepare_ly_file ?(show_meta=false) ?(meta_in_title=false) ~fname version =
 module Svg = struct
   let cache : (Version.t, string Lwt.t) Cache.t = Cache.create ()
 
-  let render version =
+  let render ?parameters version =
     Cache.use ~cache ~key:version @@ fun () ->
     Log.debug (fun m -> m "Rendering the LilyPond version");
     let%lwt (fname_ly, fname_svg) =
@@ -88,23 +100,32 @@ module Svg = struct
     Log.debug (fun m -> m "SVG file name: %s" fname_svg);
     let path = Filename.concat !Dancelor_server_config.cache "version" in
     Log.debug (fun m -> m "Preparing lilypond file");
-    prepare_ly_file ~show_meta:false ~fname:(Filename.concat path fname_ly) version; %lwt
+    prepare_ly_file ?parameters ~show_meta:false ~fname:(Filename.concat path fname_ly) version; %lwt
     Log.debug (fun m -> m "Generate score and crop");
     LilyPond.cropped_svg ~exec_path:path fname_ly; %lwt
     Log.debug (fun m -> m "done!");
     Lwt.return (Filename.concat path fname_svg)
 
-  let get version _ =
+  let get version query_parameters =
     Log.debug (fun m -> m "Version.Svg.get %a" Slug.pp version);
     let%lwt version = Version.get version in
-    let%lwt path_svg = render version in
+    let%lwt parameters =
+      match QueryParameters.get "parameters" query_parameters with
+      | None -> Lwt.return_none
+      | Some parameters ->
+        parameters
+        |> VersionParameters.of_yojson
+        |> Result.get_ok
+        |> Lwt.return_some
+    in
+    let%lwt path_svg = render ?parameters version in
     Cohttp_lwt_unix.Server.respond_file ~fname:path_svg ()
 end
 
 module Pdf = struct
   let cache : (Version.t, string Lwt.t) Cache.t = Cache.create ()
 
-  let render version =
+  let render ?parameters version =
     Cache.use ~cache ~key:version @@ fun () ->
     let%lwt (fname_ly, fname_pdf) =
       let%lwt slug = Version.slug version in
@@ -112,16 +133,25 @@ module Pdf = struct
       Lwt.return (fname^".ly", fname^".pdf")
     in
     let path = Filename.concat !Dancelor_server_config.cache "version" in
-    prepare_ly_file ~show_meta:true ~meta_in_title:true
+    prepare_ly_file ?parameters ~show_meta:true ~meta_in_title:true
       ~fname:(Filename.concat path fname_ly) version; %lwt
     Log.debug (fun m -> m "Processing with LilyPond");
     LilyPond.run ~exec_path:path fname_ly; %lwt
     Lwt.return (Filename.concat path fname_pdf)
 
-  let get version _ =
+  let get version query_parameters =
     Log.debug (fun m -> m "Version.pdf.get %a" Slug.pp version);
     let%lwt version = Version.get version in
-    let%lwt path_pdf = render version in
+    let%lwt parameters =
+      match QueryParameters.get "parameters" query_parameters with
+      | None -> Lwt.return_none
+      | Some parameters ->
+        parameters
+        |> VersionParameters.of_yojson
+        |> Result.get_ok
+        |> Lwt.return_some
+    in
+    let%lwt path_pdf = render ?parameters version in
     Cohttp_lwt_unix.Server.respond_file ~fname:path_pdf ()
 end
 
