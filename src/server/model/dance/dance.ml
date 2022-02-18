4e30dd29f28cbc286fb14b2cebd0d33ad5ab4209
Aymeric Fromherz <aymeric.fr@hotmail.fr>
2022-02-18 01:19:39 +0100

    Add make_and_save endpoints for dances

--- src/server/model/dance/dance.ml
+++ src/server/model/dance/dance.ml
@@ -4,6 +4,25 @@ include DanceLifted
 module E = Dancelor_common_model.DanceEndpoints
 module A = E.Arguments
 
+let make_and_save
+    ?status ~name ~kind ?deviser ~two_chords ?scddb_id ()
+  =
+  Dancelor_server_database.Dance.save ~slug_hint:name @@ fun slug ->
+  make ?status ~slug ~name ~kind ?deviser ~two_chords ?scddb_id ()
+
+let () =
+  Madge_server.(
+    register ~endpoint:E.make_and_save @@ fun {a} {o} ->
+    make_and_save
+      ?status:      (o A.status)
+      ~name:        (a A.name)
+      ~kind:        (a A.kind)
+      ?deviser:     (o A.deviser)
+      ~two_chords:  (a A.two_chords)
+      ?scddb_id:    (o A.scddb_id)
+      ()
+  )
+
 let search ?pagination ?(threshold=Float.min_float) filter =
   Dancelor_server_database.Dance.get_all ()
   >>=| Score.lwt_map_from_list (DanceFilter.accepts filter)
