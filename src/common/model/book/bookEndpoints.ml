2d9ea0d5dd734c285f50f91219415ae575077232
Aymeric Fromherz <aymeric.fr@hotmail.fr>
2022-05-26 01:27:19 +0200

    Add contents and parameters to book make_and_save endpoint

--- src/common/model/book/bookEndpoints.ml
+++ src/common/model/book/bookEndpoints.ml
@@ -1,9 +1,19 @@
 open Madge_common
 
+module Page_slug : Madge_common.SERIALISABLE
+  with type t = BookCore.page_slug
+  = struct
+  type t = BookCore.page_slug
+  let _key = "book-page-slug"
+  let to_yojson = BookCore.page_slug_to_yojson
+  let of_yojson = BookCore.page_slug_of_yojson
+end
+
 module Arguments = struct
   let slug = arg ~key:"slug" (module MSlug(BookCore))
   let status = optarg (module Status)
   let title = arg ~key:"title" (module MString)
+  let contents_and_parameters = optarg ~key:"contents" (module MList(Page_slug))
   let filter = arg (module BookFilter)
   let pagination = optarg (module Pagination)
   let threshold = optarg ~key:"threshold" (module MFloat)
