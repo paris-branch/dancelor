8214be617111c7198f0edb467c0754d77e4bcb69
Niols <niols@niols.fr>
2022-08-30 13:11:26 +0200

    Reformat flake for more readability

--- flake.nix
+++ flake.nix
@@ -11,30 +11,28 @@
     nixpkgs.follows = "opam-nix/nixpkgs";
   };
   outputs = { self, flake-utils, opam-nix, nixpkgs, ... }@inputs:
-    let package = "dancelor";
-    in flake-utils.lib.eachDefaultSystem (system: {
-      legacyPackages = let
-        pkgs = nixpkgs.legacyPackages.${system};
-        on = opam-nix.lib.${system};
-        scope =
-          on.buildOpamProject { } package ./. { ocaml-base-compiler = null; };
-        overlay = self: super: {
-          # Your overrides go here
-          dancelor = super.dancelor.overrideAttrs (oa: {
-            nativeBuildInputs = oa.nativeBuildInputs or [ ]
-              ++ [ pkgs.makeWrapper ];
-            postInstall =
-              "wrapProgram $out/bin/dancelor-server --prefix PATH : ${
-                pkgs.lib.makeBinPath [
-                  pkgs.inkscape
-                  pkgs.lilypond
-                  pkgs.timidity
-                  pkgs.freepats
-                ]
-              }";
-          });
-        };
-      in scope.overrideScope' overlay;
+    let package = "dancelor"; in
+    flake-utils.lib.eachDefaultSystem (system: {
+      legacyPackages =
+        let pkgs = nixpkgs.legacyPackages.${system};
+            on = opam-nix.lib.${system};
+            scope = on.buildOpamProject { } package ./. { ocaml-base-compiler = null; };
+            overlay = self: super: {
+              dancelor = super.dancelor.overrideAttrs (oa: {
+                nativeBuildInputs = oa.nativeBuildInputs or [ ] ++ [ pkgs.makeWrapper ];
+                postInstall =
+                  "wrapProgram $out/bin/dancelor-server --prefix PATH : ${
+                    pkgs.lib.makeBinPath [
+                      pkgs.inkscape
+                      pkgs.lilypond
+                      pkgs.timidity
+                      pkgs.freepats
+                    ]
+                  }";
+              });
+            };
+        in
+        scope.overrideScope' overlay;
 
       defaultPackage = self.legacyPackages.${system}.${package};
     });
