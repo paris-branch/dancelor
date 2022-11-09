2dbe6a546efb3e431466dd67cacfbb21eda4c8c5
Niols <niols@niols.fr>
2022-11-09 22:33:45 +0100

    No need for generalised fnuctions either

--- lib/nes/nesPartialDate.ml
+++ lib/nes/nesPartialDate.ml
@@ -7,35 +7,32 @@ type t =
   | YearMonth of int * int
   | Year of int
 
+let compare = compare
+
 let check_day day = day >= 1 && day <= 31
 let check_month month = month >= 1 && month <= 12
 let check_year _year = true
 
-let check_gen allow_partial = function
+let check = function
   | YearMonthDay (year, month, day) -> check_year year && check_month month && check_day day
-  | YearMonth (year, month) -> allow_partial && check_year year && check_month month
-  | Year year -> allow_partial && check_year year
+  | YearMonth (year, month) -> check_year year && check_month month
+  | Year year -> check_year year
 
-let from_string_gen check_ name s =
+let from_string s =
   let date =
     match String.split_on_char '-' s |> List.map int_of_string_opt with
     | [Some year] -> Year year
     | [Some year; Some month] -> YearMonth (year, month)
     | [Some year; Some month; Some day] -> YearMonthDay (year, month, day)
-    | _ -> failwith name
+    | _ -> failwith "NesDate.Partial.from_string"
   in
-  if not (check_ date) then failwith name;
+  if not (check date) then failwith "NesDate.Partial.from_string";
   date
-
-let check = check_gen true
-
-let from_string = from_string_gen check "NesDate.Partial.from_string"
 
 let to_string = function
   | Year year -> spf "%04d" year
   | YearMonth (year, month) -> spf "%04d-%02d" year month
   | YearMonthDay (year, month, day) -> spf "%04d-%02d-%02d" year month day
-
 
 let to_yojson date =
   `String (to_string date)
@@ -45,8 +42,6 @@ let of_yojson = function
     (try Ok (from_string s)
      with _ -> Error "NesDate.Partialof_yojson: not a valid date")
   | _ -> Error "NesDate.Partialof_yojson: not a JSON string"
-
-let compare = compare
 
 let month_to_pretty_string month =
   [| "January"; "February"; "March"; "April"; "May"; "June"; "July";
