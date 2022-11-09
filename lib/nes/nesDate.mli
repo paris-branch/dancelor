214620c6834655b8b05315e154825dadf3b6b256
Niols <niols@niols.fr>
2022-11-09 20:50:02 +0100

    `.mli` file for dates

---
+++ lib/nes/nesDate.mli
@@ -0,0 +1,13 @@
+type t [@@deriving yojson]
+type full
+
+val none : t
+val is_none : t -> bool
+
+val from_string : string -> t
+val from_string_full : string -> full
+
+val to_string : t -> string
+val to_pretty_string : ?at:bool -> t -> string
+
+val compare : t -> t -> int
