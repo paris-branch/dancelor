b22e1f4c8da863e145ea8571772b75e23eb4ca94
Niols <niols@niols.fr>
2022-11-09 21:06:46 +0100

    Better implementation of dates

--- lib/nes/nesDate.ml
+++ lib/nes/nesDate.ml
@@ -1,20 +1,29 @@
 open NesPervasives
 
-type t = int * int * int
-type full = t
+type full = [
+  | `YearMonthDay of int * int * int
+]
+type partial = [
+  | full
+  | `YearMonth of int * int
+  | `Year of int
+]
+type partial_or_none = [ partial | `None ]
 
-let none = (0, 0, 0)
-let is_none = (=) none
+type t = partial_or_none
+
+let none = `None
+let is_none = (=) `None
 
 let check_day day = day >= 1 && day <= 31
 let check_month month = month >= 1 && month <= 12
 let check_year _year = true
 
 let check_gen allow_none allow_partial = function
-  | (0, 0, 0) -> allow_none
-  | (year, 0, 0) -> allow_partial && check_year year
-  | (year, month, 0) -> allow_partial && check_year year && check_month month
-  | (year, month, day) -> check_year year && check_month month && check_day day
+  | `YearMonthDay (year, month, day) -> check_year year && check_month month && check_day day
+  | `YearMonth (year, month) -> allow_partial && check_year year && check_month month
+  | `Year year -> allow_partial && check_year year
+  | `None -> allow_none
 
 let check = check_gen true true
 let check_full = check_gen false false
@@ -22,26 +31,25 @@ let check_full = check_gen false false
 let from_string_gen check_ name s =
   let date =
     match String.split_on_char '-' s |> List.map int_of_string_opt with
-    | [Some year] -> (year, 0, 0)
-    | [Some year; Some month] -> (year, month, 0)
-    | [Some year; Some month; Some day] -> (year, month, day)
+    | [Some year] -> `Year year
+    | [Some year; Some month] -> `YearMonth (year, month)
+    | [Some year; Some month; Some day] -> `YearMonthDay (year, month, day)
     | _ -> failwith name
   in
   if not (check_ date) then failwith name;
   date
 
 let from_string = from_string_gen check "NesDate.from_string"
-let from_string_full = from_string_gen check_full "NesDate.from_string_full"
+let from_string_full str =
+  match from_string_gen check_full "NesDate.from_string_full" str with
+  | `YearMonthDay _ as date -> date
+  | _ -> assert false
 
-let to_string (year, month, day) =
-  if year = 0 then
-    ""
-  else if month = 0 then
-    spf "%04d" year
-  else if day = 0 then
-    spf "%04d-%02d" year month
-  else
-    spf "%04d-%02d-%02d" year month day
+let to_string = function
+  | `None -> ""
+  | `Year year -> spf "%04d" year
+  | `YearMonth (year, month) -> spf "%04d-%02d" year month
+  | `YearMonthDay (year, month, day) -> spf "%04d-%02d-%02d" year month day
 
 let to_yojson date =
   `String (to_string date)
@@ -58,12 +66,8 @@ let month_to_pretty_string month =
   [| "January"; "February"; "March"; "April"; "May"; "June"; "July";
      "August"; "September"; "October"; "November"; "December" |].(month - 1)
 
-let to_pretty_string ?(at=false) (year, month, day) =
-  if year = 0 then
-    ""
-  else if month = 0 then
-    spf "%s%d" (if at then "in " else "") year
-  else if day = 0 then
-    spf "%s%s %d" (if at then "in " else "") (month_to_pretty_string month) year
-  else
-    spf "%s%d %s %d" (if at then "on " else "") day (month_to_pretty_string month) year
+let to_pretty_string ?(at=false) = function
+  | `None -> ""
+  | `Year year -> spf "%s%d" (if at then "in " else "") year
+  | `YearMonth (year, month) -> spf "%s%s %d" (if at then "in " else "") (month_to_pretty_string month) year
+  | `YearMonthDay (year, month, day) -> spf "%s%d %s %d" (if at then "on " else "") day (month_to_pretty_string month) year
