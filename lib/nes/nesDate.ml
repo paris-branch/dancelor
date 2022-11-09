a64fcd9318c567bf17aec52164e178696344c343
Niols <niols@niols.fr>
2022-11-09 20:44:48 +0100

    Generalise `Date.from_string` and introduce `full`

--- lib/nes/nesDate.ml
+++ lib/nes/nesDate.ml
@@ -1,25 +1,37 @@
 open NesPervasives
 
 type t = int * int * int
+type full = t
 
 let none = (0, 0, 0)
 let is_none = (=) none
 
-let from_string s =
-  match String.split_on_char '-' s |> List.map int_of_string_opt with
-  | [Some year] ->
-    (year, 0, 0)
-  | [Some year; Some month]
-    when month >= 1 && month <= 12
-    ->
-    (year, month, 0)
-  | [Some year; Some month; Some day]
-    when month >= 1 && month <= 12
-         && day >= 1 && day <= 31
-    ->
-    (year, month, day)
-  | _ ->
-    failwith "NesDate.from_string"
+let check_day day = day >= 1 && day <= 31
+let check_month month = month >= 1 && month <= 12
+let check_year _year = true
+
+let check_gen allow_none allow_partial = function
+  | (0, 0, 0) -> allow_none
+  | (year, 0, 0) -> allow_partial && check_year year
+  | (year, month, 0) -> allow_partial && check_year year && check_month month
+  | (year, month, day) -> check_year year && check_month month && check_day day
+
+let check = check_gen true true
+let check_full = check_gen false false
+
+let from_string_gen check_ name s =
+  let date =
+    match String.split_on_char '-' s |> List.map int_of_string_opt with
+    | [Some year] -> (year, 0, 0)
+    | [Some year; Some month] -> (year, month, 0)
+    | [Some year; Some month; Some day] -> (year, month, day)
+    | _ -> failwith name
+  in
+  if not (check_ date) then failwith name;
+  date
+
+let from_string = from_string_gen check "NesDate.from_string"
+let from_string_full = from_string_gen check_full "NesDate.from_string_full"
 
 let to_string (year, month, day) =
   if year = 0 then
