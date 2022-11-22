6836579b4b2baaa25f2143e2ab6f40164d0ddb6f
Niols <niols@niols.fr>
2022-11-23 00:27:51 +0100

    Always end fname with hash
    This will fix a bug causing Dancelor to fail to even start. However,
    this is a bit fragile and we should consider a sturdier handling of the
    cache mechanism.

--- src/server/controller/version.ml
+++ src/server/controller/version.ml
@@ -188,7 +188,7 @@ module Pdf = struct
     let%lwt (fname_ly, fname_pdf) =
       let%lwt slug = Version.slug version in
       let hash = Hashtbl.hash key in
-      let fname = aspf "%a-%x-with-meta" Slug.pp slug hash in
+      let fname = aspf "%a-with-meta-%x" Slug.pp slug hash in
       Lwt.return (fname^".ly", fname^".pdf")
     in
     let path = Filename.concat !Dancelor_server_config.cache "version" in
