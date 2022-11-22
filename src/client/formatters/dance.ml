e0aa026e4f99f44eda484386beab74dcbd84f826
Niols <niols@niols.fr>
2022-11-22 14:30:11 +0100

    Use client router in formatters

--- src/client/formatters/dance.ml
+++ src/client/formatters/dance.ml
@@ -1,14 +1,14 @@
 open Dancelor_client_html
 
 module M = Dancelor_client_model
-module Router = Dancelor_common.Router
+module R = Dancelor_client_router
 
 let name ?(link=true) dance =
   let name_lwt = M.Dance.name dance in
   if link then
     let href_lwt =
       let%lwt slug = M.Dance.slug dance in
-      Lwt.return (Router.path_of_controller (Router.Dance slug) |> snd)
+      Lwt.return R.(path (Dance slug))
     in
     Lwt.return [ a ~href_lwt [ text_lwt name_lwt ] ]
   else
