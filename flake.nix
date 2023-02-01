98b67b91ee96142cfa448940bf150f1c03088481
Niols <niols@niols.fr>
2023-02-01 23:41:34 +0100

    Use flake-parts's `inputs'` instead of `inputs`

--- flake.nix
+++ flake.nix
@@ -11,8 +11,13 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
 
-      perSystem = { pkgs, system, ... }: {
+      ## Improve the way `inputs'` are computed by also handling the case of
+      ## flakes having a `lib.${system}` attribute.
+      ##
+      perInput = system: flake:
+        if flake ? lib.${system} then { lib = flake.lib.${system}; } else { };
 
+      perSystem = { inputs', self', system, pkgs, ... }: {
         ## Curate our own set of packages that will be basically opam-nix's
         ## nixpkgs with one modification: We overwrite the package `timidity` by
         ## a custom version coming from our custom github:niols/nixpkg-timidity
@@ -22,7 +27,7 @@
           inherit system;
           overlays = [
             (self: super: {
-              timidity = inputs.timidity.packages.${system}.timidityWithVorbis;
+              timidity = inputs'.timidity.packages.timidityWithVorbis;
             })
           ];
         };
@@ -30,25 +35,25 @@
         formatter = pkgs.nixfmt;
 
         packages =
-          (inputs.opam-nix.lib.${system}.buildOpamProject { pkgs = pkgs; }
-            "dancelor" ./. {
+          (inputs'.opam-nix.lib.buildOpamProject { pkgs = pkgs; } "dancelor"
+            ./. {
               merlin = "*";
               ocaml-base-compiler = "*";
               ocaml-lsp-server = "*";
               ocp-indent = "*";
               utop = "*";
             }) // {
-              default = self.packages.${system}.dancelor;
+              default = self'.packages.dancelor;
             };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = with self.packages.${system}; [
+          buildInputs = with self'.packages; [
             merlin
             ocaml-lsp-server
             ocp-indent
             utop
           ];
-          inputsFrom = [ self.packages.${system}.dancelor ];
+          inputsFrom = [ self'.packages.dancelor ];
         };
       };
     };
