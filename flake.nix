4bf834627678eba925831cda89b611cbb3fb978d
Niols <niols@niols.fr>
2023-02-01 23:21:46 +0100

    Convert to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,49 +3,53 @@
     opam-nix.url = "github:tweag/opam-nix";
     nixpkgs.follows = "opam-nix/nixpkgs";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     timidity.url = "github:niols/nixpkg-timidity";
   };
 
-  outputs = { self, nixpkgs, opam-nix, flake-utils, timidity }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-      ## Curate our own set of packages that will be basically opam-nix's
-      ## nixpkgs with one modification: We overwrite the package `timidity` by a
-      ## custom version coming from our custom github:niols/nixpkg-timidity
-      ## flake that provides a version of TiMidity++ with Ogg Vorbis support.
-      ##
-      let
-        timidityOverlay = self: super: {
-          timidity = timidity.packages.${system}.timidityWithVorbis;
-        };
-        pkgs = import nixpkgs {
+      perSystem = { pkgs, system, ... }: {
+
+        ## Curate our own set of packages that will be basically opam-nix's
+        ## nixpkgs with one modification: We overwrite the package `timidity` by
+        ## a custom version coming from our custom github:niols/nixpkg-timidity
+        ## flake that provides a version of TiMidity++ with Ogg Vorbis support.
+        ##
+        _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [ timidityOverlay ];
+          overlays = [
+            (self: super: {
+              timidity = inputs.timidity.packages.${system}.timidityWithVorbis;
+            })
+          ];
         };
 
-        on = opam-nix.lib.${system};
-
-        packages = on.buildOpamProject { pkgs = pkgs; } "dancelor" ./. {
-          merlin = "*";
-          ocaml-base-compiler = "*";
-          ocaml-lsp-server = "*";
-          ocp-indent = "*";
-          utop = "*";
-        };
-      in {
         formatter = pkgs.nixfmt;
 
-        packages = packages // { default = self.packages.${system}.dancelor; };
+        packages =
+          (inputs.opam-nix.lib.${system}.buildOpamProject { pkgs = pkgs; }
+            "dancelor" ./. {
+              merlin = "*";
+              ocaml-base-compiler = "*";
+              ocaml-lsp-server = "*";
+              ocp-indent = "*";
+              utop = "*";
+            }) // {
+              default = self.packages.${system}.dancelor;
+            };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            packages.merlin
-            packages.ocaml-lsp-server
-            packages.ocp-indent
-            packages.utop
+          buildInputs = with self.packages.${system}; [
+            merlin
+            ocaml-lsp-server
+            ocp-indent
+            utop
           ];
-          inputsFrom = [ packages.dancelor ];
+          inputsFrom = [ self.packages.${system}.dancelor ];
         };
-      });
+      };
+    };
 }
