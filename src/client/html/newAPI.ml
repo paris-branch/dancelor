c795b048d1b974b214938c74c6b35f9daf12dcf1
Niols <niols@niols.fr>
2023-09-30 11:53:14 +0000

    Add helpers for promises on a loop
    For analogy with the existing ones (and to explain why `from'` has a
    quote). They will prove useful in the future.

--- src/client/html/newAPI.ml
+++ src/client/html/newAPI.ml
@@ -22,7 +22,21 @@ module S = struct
   include Lwt_react.S
 
   (** [from' ~placeholder promise] creates a signal that holds the [placeholder]
-      until the [promise] resolves. *)
+      until the [promise] resolves, and changes every time [promise] resolves
+      again after that. *)
+  let from (placeholder: 'a) (promise : unit -> 'a Lwt.t) : 'a Lwt_react.signal =
+    let result, send_result = create placeholder in
+    let rec loop () =
+      Lwt.bind (promise ()) @@ fun result ->
+      send_result result;
+      loop ()
+    in
+    Lwt.async loop;
+    result
+
+  (** [from' ~placeholder promise] creates a signal that holds the [placeholder]
+      until the [promise] resolves. This is similar to {!from} except it does
+      only one update. *)
   let from' (placeholder: 'a) (promise : 'a Lwt.t) : 'a Lwt_react.signal =
     let result, send_result = create placeholder in
     Lwt.on_success promise send_result;
@@ -32,6 +46,14 @@ end
 (** Reactive lists. *)
 module RList = struct
   include ReactiveData.RList
+
+  (** [from_lwt placeholder promise] is a container whose initial value is
+      [placeholder], and which gets updated every time [promise] resolves. When
+      that happens we detect the differences between the previous value and
+      [promise]'s result, and perform downstream computation (e.g., for [map])
+      only on the new and modified elements. *)
+  let from_lwt placeholder promise =
+    from_signal @@ S.from placeholder promise
 
   (** [from_lwt' placeholder promise] is a container whose initial value is
       [placeholder], and which gets updated once [promise] resolves. When that
