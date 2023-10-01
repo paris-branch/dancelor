4ff53fecb9df990fc752afdbaf508da3c8b80222
Niols <niols@niols.fr>
2023-10-01 13:42:43 +0000

    Fully featured page navigation widget

--- src/client/elements/pageNavNewAPI.ml
+++ src/client/elements/pageNavNewAPI.ml
@@ -3,7 +3,7 @@ open Dancelor_common_model
 open Dancelor_client_html.NewAPI
 
 type state = {
-  current_page : int; (* first page is [0] *)
+  current_page : int; (* first page is [1] *)
   entries_per_page : int;
   number_of_entries : int;
 }
@@ -15,9 +15,10 @@ let number_of_pages state =
   / state.entries_per_page
 
 let current_pagination state =
+  (* NOTE: Our page numbers start at 1. *)
   Pagination.{
-    start = state.current_page * state.entries_per_page;
-    end_ = (state.current_page + 1) * state.entries_per_page;
+    start = (state.current_page - 1) * state.entries_per_page;
+    end_ = min (state.current_page * state.entries_per_page) state.number_of_entries;
   }
 
 type t = {
@@ -28,7 +29,7 @@ type t = {
 
 let create ~number_of_entries ~entries_per_page =
   let initial = {
-    current_page = 0;
+    current_page = 1;
     entries_per_page;
     number_of_entries = 0;
   }
@@ -50,38 +51,104 @@ let status_text pagination =
       (Pagination.end_ pagination)
       state.number_of_entries
 
-let button page text pagination =
-  li
-    ~a:[
-      R.a_class (
-        flip S.map pagination.signal @@ fun state ->
-        if page = state.current_page then ["active"] else []
-      )
-    ]
-    [
-      button ~a: [
-        a_button_type `Button;
-        a_onclick
-          (fun _ ->
-             pagination.updater (fun state -> { state with current_page = page });
-             false)
+module Button = struct
+  (** [make ~active ~enabled ~target ~text pagination] is a button that shows
+      [text]. It is active (that is, it shows as different from the other) when
+      the predicate [active] returns [true] on the [pagination] state. It is
+      enabled (that is not grayed out and clickable) when the predicate [enabled]
+      returns [true] on the [pagination] state. It changes the page to the result
+      of [target] applied on the [pagination] state. *)
+  let make ~active ~enabled ~target ~text pagination =
+    li
+      ~a:[
+        R.a_class (
+          flip S.map pagination.signal @@ fun state ->
+          if active state then ["active"] else []
+        )
       ]
-        [txt text]
-    ]
+      [
+        button ~a: [
+          a_button_type `Button;
+          R.a_class (
+            flip S.map pagination.signal @@ fun state ->
+            if enabled state then ["clickable"] else ["disabled"]
+          );
+          a_onclick
+            (fun _ ->
+               let state = S.value pagination.signal in
+               if enabled state then
+                 pagination.updater (fun state ->
+                     { state with current_page = target state }
+                   );
+               false
+            )
+        ]
+          [txt text]
+      ]
+
+  (** A button that is never enabled and shows three dots. *)
+  let ellipsis =
+    make
+      ~active:(Fun.const false)
+      ~enabled:(Fun.const false)
+      ~target:(fun _ -> assert false)
+      ~text:"..."
+
+  (** A button that is constantly linked to a page number. *)
+  let numbered page =
+    make
+      ~active:(fun state -> state.current_page = page)
+      ~enabled:(Fun.const true)
+      ~target:(Fun.const page)
+      ~text:(string_of_int page)
+
+  (** A button that brings to the previous page. *)
+  let previous =
+    make
+      ~active:(Fun.const false)
+      ~enabled:(fun state -> state.current_page <> 1)
+      ~target:(fun state -> state.current_page - 1)
+      ~text:"Previous"
+
+  (** A button that brings to the next page. *)
+  let next =
+    make
+      ~active:(Fun.const false)
+      ~enabled:(fun state -> state.current_page <> number_of_pages state)
+      ~target:(fun state -> state.current_page + 1)
+      ~text:"Next"
+end
 
 let button_list pagination =
   flip S.map pagination.signal @@ fun state ->
-  [
-    button 0 "First" pagination
-  ]
-  @
-  List.init
-    (number_of_pages state)
-    (fun page -> button page (string_of_int (1 + page)) pagination)
-  @
-  [
-    button (number_of_pages state - 1) "Last" pagination
-  ]
+  let number_of_pages = number_of_pages state
+  and current_page = state.current_page
+  in
+  (* Select the page numbers to show. *)
+  let relevant_page_numbers =
+    number_of_pages
+    |> flip List.init ((+) 1)
+    |> List.filter
+      (fun i ->
+         i = 1
+         || i = number_of_pages
+         || abs (current_page - i) <= 2
+      )
+  in
+  let rec numbered_buttons previous = function
+    | [] -> []
+    | page_number :: page_numbers ->
+      (if page_number <> previous + 1 then [Button.ellipsis] else [])
+      @ [Button.numbered page_number]
+      @ numbered_buttons page_number page_numbers
+  in
+  List.map
+    (fun button -> button pagination)
+    (
+      [Button.previous]
+      @ numbered_buttons 0 relevant_page_numbers @
+      [Button.next]
+    )
 
 let render pagination =
   div ~a:[a_id "page_nav"] [
