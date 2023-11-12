1ab6dafa29e5c15004e113fa448e1d791b50271e
Niols <niols@niols.fr>
2023-11-12 18:06:48 +0000

    Prefix by `Dancelor_server_model` explicitly

--- src/server/controller/set.ml
+++ src/server/controller/set.ml
@@ -1,20 +1,20 @@
 open NesUnix
-open Dancelor_server_model
+module Model = Dancelor_server_model
 module Log = (val Dancelor_server_logs.create "controller.set" : Logs.LOG)
 
 module Ly = struct
-  let cache : ([`Ly] * Set.t * SetParameters.t * string, string Lwt.t) StorageCache.t = StorageCache.create ()
+  let cache : ([`Ly] * Model.Set.t * Model.SetParameters.t * string, string Lwt.t) StorageCache.t = StorageCache.create ()
 
-  let render ?(parameters=SetParameters.none) set =
-    let%lwt body = Set.lilypond_content_cache_key set in
+  let render ?(parameters=Model.SetParameters.none) set =
+    let%lwt body = Model.Set.lilypond_content_cache_key set in
     StorageCache.use ~cache ~key:(`Ly, set, parameters, body) @@ fun _hash ->
-    let parameters = SetParameters.fill parameters in
+    let parameters = Model.SetParameters.fill parameters in
     let (res, prom) =
       Format.with_formatter_to_string_gen @@ fun fmt ->
-      let%lwt title = Set.name set in
-      let title = Option.value ~default:title @@ SetParameters.display_name parameters in
-      let%lwt kind = Set.kind set in
-      let%lwt versions_and_parameters = Set.versions_and_parameters set in
+      let%lwt title = Model.Set.name set in
+      let title = Option.value ~default:title @@ Model.SetParameters.display_name parameters in
+      let%lwt kind = Model.Set.kind set in
+      let%lwt versions_and_parameters = Model.Set.versions_and_parameters set in
       fpf fmt [%blob "template/lyversion.ly"];
       fpf fmt [%blob "template/layout.ly"];
       fpf fmt [%blob "template/paper.ly"];
@@ -24,50 +24,50 @@ module Ly = struct
       fpf fmt [%blob "template/bar-numbering/bar-number-in-instrument-name-engraver.ly"];
       fpf fmt [%blob "template/bar-numbering/beginning-of-line.ly"];
       fpf fmt [%blob "template/set/header.ly"]
-        title (Kind.Dance.to_string kind)
-        (Option.unwrap_or ~default:"" (SetParameters.instruments parameters));
+        title (Model.Kind.Dance.to_string kind)
+        (Option.unwrap_or ~default:"" (Model.SetParameters.instruments parameters));
       Lwt_list.iter_s
         (fun (version, version_parameters) ->
-           let version_parameters = VersionParameters.compose (SetParameters.every_version parameters) version_parameters in
-           let%lwt content = Version.content version in
+           let version_parameters = Model.VersionParameters.compose (Model.SetParameters.every_version parameters) version_parameters in
+           let%lwt content = Model.Version.content version in
            let content =
-             match version_parameters |> VersionParameters.clef with
+             match version_parameters |> Model.VersionParameters.clef with
              | None -> content
              | Some clef_parameter ->
                let clef_regex = Str.regexp "\\\\clef *\"?[a-z]*\"?" in
-               Str.global_replace clef_regex ("\\clef " ^ Music.clef_to_lilypond_string clef_parameter) content
+               Str.global_replace clef_regex ("\\clef " ^ Model.Music.clef_to_lilypond_string clef_parameter) content
            in
-           let%lwt key = Version.key version in
-           let%lwt tune = Version.tune version in
-           let%lwt name = Tune.name tune in
+           let%lwt key = Model.Version.key version in
+           let%lwt tune = Model.Version.tune version in
+           let%lwt name = Model.Tune.name tune in
            let name =
              version_parameters
-             |> VersionParameters.display_name
+             |> Model.VersionParameters.display_name
              |> Option.unwrap_or ~default:name
            in
            let%lwt author =
-             match%lwt Tune.author tune with
+             match%lwt Model.Tune.author tune with
              | None -> Lwt.return ""
-             | Some author -> Credit.line author
+             | Some author -> Model.Credit.line author
            in
            let author =
              version_parameters
-             |> VersionParameters.display_author
+             |> Model.VersionParameters.display_author
              |> Option.unwrap_or ~default:author
            in
            let first_bar =
              version_parameters
-             |> VersionParameters.first_bar
+             |> Model.VersionParameters.first_bar
            in
            let source, target =
-             match version_parameters |> VersionParameters.transposition with
+             match version_parameters |> Model.VersionParameters.transposition with
              | Relative (source, target) -> (source, target)
-             | Absolute target -> (Music.key_pitch key, target) (* FIXME: probably an octave to fix here*)
+             | Absolute target -> (Model.Music.key_pitch key, target) (* FIXME: probably an octave to fix here*)
            in
            fpf fmt [%blob "template/set/version.ly"]
              name author first_bar
-             (Music.pitch_to_lilypond_string source)
-             (Music.pitch_to_lilypond_string target)
+             (Model.Music.pitch_to_lilypond_string source)
+             (Model.Music.pitch_to_lilypond_string target)
              content;
            Lwt.return ())
         versions_and_parameters
@@ -101,19 +101,19 @@ let populate_cache ~cache ~ext ~pp_ext =
     ) files
 
 module Pdf = struct
-  let cache : ([`Pdf] * Set.t * SetParameters.t option * string, string Lwt.t) StorageCache.t =
+  let cache : ([`Pdf] * Model.Set.t * Model.SetParameters.t option * string, string Lwt.t) StorageCache.t =
     StorageCache.create ()
 
   let populate_cache () =
     populate_cache ~cache ~ext:".pdf" ~pp_ext:"pdf"
 
   let render ?parameters set =
-    let%lwt body = Set.lilypond_content_cache_key set in
+    let%lwt body = Model.Set.lilypond_content_cache_key set in
     StorageCache.use ~cache ~key:(`Pdf, set, parameters, body) @@ fun hash ->
     let%lwt lilypond = Ly.render ?parameters set in
     let path = Filename.concat !Dancelor_server_config.cache "set" in
     let%lwt (fname_ly, fname_pdf) =
-      let%lwt slug = Set.slug set in
+      let%lwt slug = Model.Set.slug set in
       let fname = aspf "%a-%a" Slug.pp slug StorageCache.pp_hash hash in
       Lwt.return (fname^".ly", fname^".pdf")
     in
@@ -125,7 +125,7 @@ module Pdf = struct
     Lwt.return path_pdf
 
   let get set parameters =
-    let%lwt set = Set.get set in
+    let%lwt set = Model.Set.get set in
     let%lwt path_pdf = render ?parameters set in
     Cohttp_lwt_unix.Server.respond_file ~fname:path_pdf ()
 end
