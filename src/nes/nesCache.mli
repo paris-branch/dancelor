fa153efd7d29018f3ac5c9a673b4f5f04b1a25f9
Niols <niols@niols.fr>
2024-01-27 21:26:24 +0000

    Support lifetime in cache

--- src/nes/nesCache.mli
+++ src/nes/nesCache.mli
@@ -1,9 +1,21 @@
 (** {1 Cache} *)
 
-type ('k, 'v) t
+type ('key, 'value) t
+(** Type of a cache binding ['key]s to ['value]s. *)
 
-val create : unit -> ('k, 'v) t
+val create : ?lifetime:int -> unit -> ('key, 'value) t
+(** Create a cache. The [?lifetime] argument allows to specify (in seconds) when
+    bindings from the cache should be invalidated. Entries are _actually_
+    removed from the cache during normal {!use} or when explicitly calling for a
+    {!cleanup}. *)
 
-val use : cache:('k, 'v) t -> key:'k -> (unit -> 'v) -> 'v
+val use : cache:('key, 'value) t -> key:'key -> (unit -> 'value) -> 'value
+(** Looks up the [~key] in the [~cache]. If it exists, return its value.
+    Otherwise, call the given function, store its result and return it. *)
 
-val remove : cache:('k, 'v) t -> key:'k -> unit
+val remove : cache:('key, 'value) t -> key:'key -> unit
+(** Remove a [~key] from the [~cache]. *)
+
+val cleanup : cache:('key, 'value) t -> unit
+(** Explicitly call for a cache clean-up, removing entries that have passed
+    their lifetime. *)
