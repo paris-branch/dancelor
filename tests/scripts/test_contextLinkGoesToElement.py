381b2a9ad13602ddf22116f96a3f4a906760efbe
Niols <niols@niols.fr>
2024-01-27 22:23:46 +0000

    Add test to travel between elements with keys

--- tests/scripts/test_contextLinkGoesToElement.py
+++ tests/scripts/test_contextLinkGoesToElement.py
@@ -20,8 +20,14 @@ class TestContextLinkGoesToElement():
   def teardown_method(self, method):
     self.driver.quit()
 
-  def test_contextLinkGoesToElement(self):
+  def test_contextLinkGoesToElementWithClick(self):
     self.driver.get("http://localhost:8080/set/tam-lin-thrice?in-search=%22tam%22")
     self.driver.find_element(By.CSS_SELECTOR, ".context-link-left .context-link-main").click()
+    assert self.driver.find_element(By.CSS_SELECTOR, ".title:nth-child(1)").text == "The Tam Lin Book"
+    assert self.driver.title == "The Tam Lin Book | Book | Dancelor"
+
+  def test_contextLinkGoesToElementWithArrow(self):
+    self.driver.get("http://localhost:8080/set/tam-lin-thrice?in-search=%22tam%22")
+    self.driver.find_element(By.CSS_SELECTOR, "body").send_keys(Keys.LEFT)
     assert self.driver.find_element(By.CSS_SELECTOR, ".title:nth-child(1)").text == "The Tam Lin Book"
     assert self.driver.title == "The Tam Lin Book | Book | Dancelor"
