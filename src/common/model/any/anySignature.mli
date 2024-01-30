2dc35249445b917430c01d2e8a782c1c4a62af38
Niols <niols@niols.fr>
2024-01-30 21:00:02 +0000

    Document and export everything in `AnySignature`

--- src/common/model/any/anySignature.mli
+++ src/common/model/any/anySignature.mli
@@ -1,41 +1,128 @@
+(** {1 Any} *)
+
 open Nes
 
-type t = AnyCore.t
+type t = [%import: AnyCore.t]
+(** Type of an “any” element, that is simply a sum type of all the other
+    models. *)
+
+(** {3 Constructors} *)
 
 val person : PersonCore.t -> t
+(** Function equivalent of the [Person] constructor. *)
+
 val dance : DanceCore.t -> t
+(** Function equivalent of the [Dance] constructor. *)
+
 val book : BookCore.t -> t
+(** Function equivalent of the [Book] constructor. *)
+
 val set : SetCore.t -> t
+(** Function equivalent of the [Set] constructor. *)
+
 val tune : TuneCore.t -> t
+(** Function equivalent of the [Tune] constructor. *)
+
 val version : VersionCore.t -> t
+(** Function equivalent of the [Version] constructor. *)
+
+(** {3 Destructors} *)
 
 val equal : t -> t -> bool
+(** Equality between two {!t}. *)
 
 val name : t -> string Lwt.t
 (** Finds a name to give to the element, no matter what it is. *)
 
+(** {2 Type} *)
+
 module Type : sig
   type t = [%import: AnyCore.Type.t]
+  (** Type to represent the type of an “any”. There is basically one type per
+      model, eg. [Version] or [Dance]. Must not be mistaken for a kind, which,
+      in Dancelor parlance, is eg. [Reel] or [8 x 32 Strathspey]. *)
 
   val all : t list
+  (** All the existing types, as a list. There is no guarantee on the order in
+      which those elements appear. *)
+
   val to_string : t -> string
+  (** Convert a type to a string, eg [to_string Person = "Person"]. *)
+
+  exception NotAType of string
+  (** See {!of_string}. *)
+
+  val of_string : string -> t
+  (** Convert a string to a type, eg. [of_string "person" = Person]. If the
+      string is not a valid representation of a type, raises {!NotAType}. *)
+
+  val of_string_opt : string -> t option
+  (** Option equivalent of {!of_string}. *)
 end
 
 val type_of : t -> Type.t
+(** Get the type of an “any” element. *)
+
+(** {2 Filters} *)
 
 module Filter : sig
   type predicate = [%import: AnyCore.Filter.predicate]
-  type t = [%import: AnyCore.Filter.t]
+  (** Type of predicates on “any” elements. *)
 
-  val type_ : Type.t -> t
+  type t = [%import: AnyCore.Filter.t]
+  (** Type of a filter on “any” element, that is a formula over
+      {!predicate}s. *)
 
   val accepts : t -> AnyCore.t -> float Lwt.t
+  (** Whether the given filter accepts the given element. *)
+
+  (** {3 Constructors} *)
+
+  val type_ : Type.t -> t
+  (** A filter that asserts that the element has the given type. *)
+
+  val asPerson : PersonSignature.Filter.t -> t
+  (** Lift a filter on persons to make a filter on “any”. This filter asserts
+      that the “any” element is a person that matches the given filter. *)
+
+  val asDance : DanceSignature.Filter.t -> t
+  (** Lift a filter on dances to make a filter on “any”. This filter asserts
+      that the “any” element is a dance that matches the given filter. *)
+
+  val asBook : BookSignature.Filter.t -> t
+  (** Lift a filter on books to make a filter on “any”. This filter asserts that
+      the “any” element is a book that matches the given filter. *)
+
+  val asSet : SetSignature.Filter.t -> t
+  (** Lift a filter on sets to make a filter on “any”. This filter asserts that
+      the “any” element is a set that matches the given filter. *)
+
+  val asTune : TuneSignature.Filter.t -> t
+  (** Lift a filter on tunes to make a filter on “any”. This filter asserts that
+      the “any” element is a tune that matches the given filter. *)
+
+  val asVersion : VersionSignature.Filter.t -> t
+  (** Lift a filter on versions to make a filter on “any”. This filter asserts
+      that the “any” element is a version that matches the given filter. *)
+
+  (** {3 Destructors} *)
 
   val from_string : string -> (t, string list) result
+  (** Parse a text formula into a filter on “any” elements. *)
+
   val from_string_exn : string -> t
+  (** Exceptional equivalent of {!from_string}. *)
+
+  (** {3 Others} *)
 
   val possible_types : t -> Type.t list
+  (** Returns the list of types that a formula can match. It is guaranteed that
+      elements whose types are not in the list will be rejected by the filter.
+      It is not guaranteed that for each type in the list there will be an
+      element of this type accepted by the filter. *)
 end
+
+(** {2 Search} *)
 
 val search :
   ?pagination:Pagination.t ->
