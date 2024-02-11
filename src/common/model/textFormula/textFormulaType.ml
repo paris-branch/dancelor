0206d3eed25a01fb2805dc090118958a08683510
Niols <niols@niols.fr>
2024-02-11 12:22:27 +0100

    Proper generation of text formulas

--- src/common/model/textFormula/textFormulaType.ml
+++ src/common/model/textFormula/textFormulaType.ml
@@ -17,12 +17,28 @@ let raw' = pred % raw
 let nullary' = pred % nullary
 let unary' p e = pred (unary p e)
 
-let gen_predicate = let open QCheck.Gen in
-  frequency
-    [(1, (raw <$> string_printable));
-     (1, (nullary <$> string_size ~gen:(char_range 'a' 'z') (int_range 1 10)))]
-(* (1, (unary <$> string_size ~gen:(char_range 'a' 'z') (int_range 1 10)) <*> Formula.gen gen_predicate))] *)
-(* FIXME: reactivate last case *)
+let gen_predicate_name =
+  let open QCheck.Gen in
+  fix
+    (fun self () ->
+       let* name = string_size ~gen:(char_range 'a' 'z') (int_range 1 10) in
+       if name = "or" || name = "and" || name = "not" then
+         self ()
+       else
+         pure name)
+    ()
+
+let gen_predicate =
+  let open QCheck.Gen in
+  fix
+    (fun self () ->
+       oneof [
+         (raw <$> string_printable);
+         (nullary <$> gen_predicate_name);
+         (unary <$> gen_predicate_name <*> Formula.gen (self ()));
+       ]
+    )
+    ()
 
 let gen = Formula.gen gen_predicate
 
