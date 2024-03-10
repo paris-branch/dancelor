79b44ef40d2fe5f68edb150e4dcce863f8de0710
Niols <niols@niols.fr>
2024-03-10 19:21:44 +0000

    Get rid of the `Score` module

--- src/server/model/search.ml
+++ src/server/model/search.ml
@@ -1,6 +1,33 @@
 open Nes
 
-module Score = Dancelor_common.Model.Score
+type 'a t =
+  { score : float ;
+    value : 'a }
+
+let lwt_map_from_list score =
+  Lwt_list.map_s
+    (fun value ->
+       let%lwt score = score value in
+       Lwt.return { score; value })
+
+let list_filter_threshold threshold =
+  List.filter (fun score -> score.score >= threshold)
+
+let list_proj_sort_decreasing compares l =
+  let compares =
+    List.map
+      (fun compare ->
+         fun x y -> compare x.value y.value)
+      compares
+  in
+  let compares =
+    (Lwt_list.decreasing (fun x -> Lwt.return x.score) compare)
+    :: compares
+  in
+  Lwt_list.sort_multiple compares l
+
+let list_erase l =
+  List.map (fun score -> score.value) l
 
 let search
     ~cache
@@ -15,10 +42,10 @@ let search
   let%lwt results =
     Cache.use ~cache ~key:(threshold, filter) @@ fun () ->
     values_getter ()
-    >>=| Score.lwt_map_from_list (scoring_function filter)
-    >>=| (Score.list_filter_threshold threshold ||> Lwt.return)
-    >>=| (Score.list_proj_sort_decreasing tiebreakers)
-    >>=| (Lwt.return % Score.list_erase)
+    >>=| lwt_map_from_list (scoring_function filter)
+    >>=| (list_filter_threshold threshold ||> Lwt.return)
+    >>=| (list_proj_sort_decreasing tiebreakers)
+    >>=| (Lwt.return % list_erase)
   in
   Lwt.return (
     List.length results,
