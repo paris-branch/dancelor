1ab8008896cf8ea1195d2d95986eaee77c3650e1
Niols <niols@niols.fr>
2024-03-18 11:09:51 +0000

    Follow-up in tests

--- tests/scripts/test_contextLinks.py
+++ tests/scripts/test_contextLinks.py
@@ -62,19 +62,20 @@ class TestContextLinks():
     ## From the set “Tam Lin Thrice” in the context of a search for “tam”, check
     ## that pressing the left key goes to “The Tam Lin Book”.
     self.driver.get("http://localhost:8080/set/tam-lin-thrice?context=%5B%22InSearch%22%2C%22tam%22%5D")
+    time.sleep(1) ## wait for page to be loaded
     self.driver.find_element(By.CSS_SELECTOR, "body").send_keys(Keys.LEFT)
     self.wait.until(EC.url_to_be("http://localhost:8080/version/tam-lin-niols?context=%5B%22InSearch%22%2C%22tam%22%5D"))
 
-  def test_bannerUpwardsTriangleGoesToContext(self):
+  def test_bannerUndoGoesToContext(self):
     ## From the set “Tam Lin Thrice” in the context of a search for “tam”, check
-    ## that clicking the banner's upwards triangle goes back to the explorer.
+    ## that clicking the banner's “undo” icon goes back to the explorer.
     self.driver.get("http://localhost:8080/set/tam-lin-thrice?context=%5B%22InSearch%22%2C%22tam%22%5D")
-    self.driver.find_element(By.LINK_TEXT, "▴").click()
+    self.driver.find_element(By.LINK_TEXT, "undo").click()
     self.wait.until(EC.url_to_be("http://localhost:8080/explore?q=%22tam%22"))
 
-  def test_bannerCrossRemovesContext(self):
+  def test_bannerCloseRemovesContext(self):
     ## From the set “Tam Lin Thrice” in the context of a search for “tam”, check
-    ## that clicking the banner's cross removes the context.
+    ## that clicking the banner's “close” icon removes the context.
     self.driver.get("http://localhost:8080/set/tam-lin-thrice?context=%5B%22InSearch%22%2C%22tam%22%5D")
-    self.driver.find_element(By.LINK_TEXT, "⨉").click()
+    self.driver.find_element(By.LINK_TEXT, "close").click()
     self.wait.until(EC.url_to_be("http://localhost:8080/set/tam-lin-thrice"))
--- tests/scripts/test_explorerComplexFilter.py
+++ tests/scripts/test_explorerComplexFilter.py
@@ -23,12 +23,12 @@ class TestExplorerComplexFilter():
 
   def test_explorerComplexFilter(self):
     self.driver.get("http://localhost:8080/explore?q=%22tam%22")
-    self.driver.find_element(By.LINK_TEXT, "Complex filter").click()
+    self.driver.find_element(By.LINK_TEXT, "filter_alt Complex filter").click()
     self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(8)").click()
     self.driver.find_element(By.LINK_TEXT, "Apply").click()
     self.driver.find_element(By.CSS_SELECTOR, "body").click()
     self.wait.until(EC.text_to_be_present_in_element((By.XPATH, "//div[@id=\'page_nav\']/div"), "Showing 1 to 1 of 1 entries"))
-    self.driver.find_element(By.LINK_TEXT, "Complex filter").click()
+    self.driver.find_element(By.LINK_TEXT, "filter_alt Complex filter").click()
     self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(10)").click()
     self.driver.find_element(By.CSS_SELECTOR, "div:nth-child(2) label:nth-child(8)").click()
     self.driver.find_element(By.CSS_SELECTOR, "div:nth-child(2) label:nth-child(10)").click()
