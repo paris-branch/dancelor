c70898051782bb1394ac5593fa8a2e8fe7e9484c
Niols <niols@niols.fr>
2024-04-10 22:45:10 +0000

    Split `make_and_render` in two

--- src/client/components/quickSearchBar.ml
+++ src/client/components/quickSearchBar.ml
@@ -16,14 +16,27 @@ let fa_row icon message =
    providing a [make_result] that gives a clickable row. That way, we could
    improve the search bar to work with keys and not just clicks. *)
 
-let make_and_render ?(number_of_results=10) ~placeholder ~search ~make_result ?on_enter ?autofocus () =
+type 'result t = {
+  min_characters : int;
+  search_bar : 'result SearchBar.t;
+}
+
+let make ?(number_of_results=10) ~search () =
   let min_characters = 3 in
   let slice = S.const @@ Slice.make ~start:0 ~end_excl:number_of_results () in
+  let search_bar =
+    SearchBar.make
+      ~search
+      ~min_characters
+      ~slice
+      ()
+  in
+  {min_characters; search_bar}
+
+let render ~placeholder ~make_result ?on_enter ?autofocus q =
 
   (** A signal tracking whether the table is focused. *)
   let (table_visible, set_table_visible) = S.create false in
-
-  let search_bar = SearchBar.make ~search ~min_characters ~slice () in
 
   div ~a:[a_class ["search-bar"]] [
     div ~a:[
@@ -35,7 +48,7 @@ let make_and_render ?(number_of_results=10) ~placeholder ~search ~make_result ?o
       a_onclick (fun _ -> set_table_visible false; false);
     ] [];
 
-    SearchBar.render ~placeholder ~on_focus:(fun () -> set_table_visible true) ?on_enter ?autofocus search_bar;
+    SearchBar.render ~placeholder ~on_focus:(fun () -> set_table_visible true) ?on_enter ?autofocus q.search_bar;
 
     tablex
       ~a:[
@@ -47,9 +60,9 @@ let make_and_render ?(number_of_results=10) ~placeholder ~search ~make_result ?o
       ]
       [
         R.tbody (
-          S.bind_s' (SearchBar.state search_bar) [] @@ function
+          S.bind_s' (SearchBar.state q.search_bar) [] @@ function
           | StartTyping -> Lwt.return [fa_row "keyboard" "Start typing to search."]
-          | ContinueTyping -> Lwt.return [fa_row "keyboard" (spf "Type at least %s characters." (Int.to_english_string min_characters))]
+          | ContinueTyping -> Lwt.return [fa_row "keyboard" (spf "Type at least %s characters." (Int.to_english_string q.min_characters))]
           | NoResults -> Lwt.return [fa_row "warning" "Your search returned no results."]
           | Errors error -> Lwt.return [fa_row "error" error]
           | Results results ->
@@ -62,3 +75,6 @@ let make_and_render ?(number_of_results=10) ~placeholder ~search ~make_result ?o
         );
       ]
   ]
+
+let make_and_render ?number_of_results ~placeholder ~search ~make_result ?on_enter ?autofocus () =
+  render ~placeholder ~make_result ?on_enter ?autofocus (make ?number_of_results ~search ())
--- src/client/components/quickSearchBar.mli
+++ src/client/components/quickSearchBar.mli
@@ -1,5 +1,34 @@
+(** {1 Quick Search Bar}
+
+    This is a wrapper around {!SearchBar}; a search bar with a floating table.
+    The table reports hints or errors and shows the search results. The minimum
+    number of characters is set to [3] and the slice picks only one page of [10]
+    entries. *)
+
 open Js_of_ocaml_tyxml.Tyxml_js
 open Dancelor_client_model
+
+type 'result t
+(** Abstract type of a quick search bar, holding results of type ['result]. *)
+
+val make :
+  ?number_of_results: int ->
+  search: (Slice.t -> string -> (int * 'result list, string) result Lwt.t) ->
+  unit ->
+  'result t
+(** Wrapper around {!SearchBar.make}; refer to it for a description of the
+    arguments. *)
+
+val render :
+  placeholder: string ->
+  make_result: ('result -> Html_types.tr Html.elt Lwt.t) ->
+  ?on_enter: (string -> unit) ->
+  ?autofocus: bool ->
+  'result t ->
+  [> Html_types.div] Html.elt
+(** Wrapper around {!SearchBar.render}; refer to it for a description of the
+    arguments. Additionally, [make_result] is a function that, from a result,
+    returns a table line displaying that result. *)
 
 val make_and_render :
   ?number_of_results: int ->
@@ -10,15 +39,4 @@ val make_and_render :
   ?autofocus:bool ->
   unit ->
   [> Html_types.div] Html.elt
-(** Wrapper around {!SearchBar.make} and {!SearchBar.render} returning a quick
-    search bar. This is a search bar with a floating table. The table reports
-    hints or errors and shows the search results. The minimum number of
-    characters is set to [3] and the slice picks only one page of [10] entries.
-
-    The arguments are mostly inherited from {!SearchBar.make} and
-    {!SearchBar.render}; refer to these functions for documentation. The
-    additional ones are to be used as follows:
-
-    - [make_result] is a function that, from a result, returns a table line
-       displaying that result;
-*)
+(** Short form of the composition of {!make} directly followed by {!render}. *)
