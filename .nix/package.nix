df69958e3f11815757a0a8670a12a82c82e7893b
Niols <niols@niols.fr>
2024-10-23 11:13:24 +0200

    Apply `nixfmt-rfc-style`

--- .nix/package.nix
+++ .nix/package.nix
@@ -1,42 +1,48 @@
 {
-  perSystem = { pkgs, ... }: {
-    packages.default = pkgs.ocamlPackages.buildDunePackage {
-      pname = "dancelor";
-      version = "dev";
-      src = ../.;
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.default = pkgs.ocamlPackages.buildDunePackage {
+        pname = "dancelor";
+        version = "dev";
+        src = ../.;
 
-      nativeBuildInputs = (with pkgs.ocamlPackages; [ menhir js_of_ocaml ])
-        ++ (with pkgs; [ sassc ]);
+        nativeBuildInputs =
+          (with pkgs.ocamlPackages; [
+            menhir
+            js_of_ocaml
+          ])
+          ++ (with pkgs; [ sassc ]);
 
-      buildInputs = with pkgs.ocamlPackages; [
-        cohttp-lwt-jsoo
-        cohttp-lwt-unix
-        dates_calc
-        iso8601
-        js_of_ocaml-ppx
-        js_of_ocaml-tyxml
-        logs
-        lwt_ppx
-        lwt_react
-        ppx_blob
-        ppx_deriving_yojson
-        ppx_import
-        ppx_inline_test
-        ppx_fields_conv
-        ppx_monad
-        slug
-        yaml
+        buildInputs = with pkgs.ocamlPackages; [
+          cohttp-lwt-jsoo
+          cohttp-lwt-unix
+          dates_calc
+          iso8601
+          js_of_ocaml-ppx
+          js_of_ocaml-tyxml
+          logs
+          lwt_ppx
+          lwt_react
+          ppx_blob
+          ppx_deriving_yojson
+          ppx_import
+          ppx_inline_test
+          ppx_fields_conv
+          ppx_monad
+          slug
+          yaml
 
-        # documentation and tests
-        alcotest
-        odoc
-        ppx_deriving_qcheck
-        qcheck
-        qcheck-alcotest
+          # documentation and tests
+          alcotest
+          odoc
+          ppx_deriving_qcheck
+          qcheck
+          qcheck-alcotest
 
-        # used for vendored `ppx_variants_conv`
-        variantslib
-      ];
+          # used for vendored `ppx_variants_conv`
+          variantslib
+        ];
+      };
     };
-  };
 }
