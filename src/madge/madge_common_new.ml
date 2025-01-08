c9651e883fa742b0ceb8e7525ad1444fe96c5883
Niols <niols@niols.fr>
2025-01-08 19:09:04 +0100

    Cleanup Madge server's side; move to common

--- src/madge/madge_common_new.ml
+++ src/madge/madge_common_new.ml
@@ -85,6 +85,74 @@ module Route = struct
       a
     = fun route return ->
       process "" Madge_query.empty route return
+
+  (* NOTE: The [controller] is in a thunk to avoid it being ran halfway as we find
+     its last argument. It is actually run at the end when all is green. *)
+  let rec match_
+    : type a w r z. (a, w, r) t ->
+      (unit -> a) ->
+      string list ->
+      Madge_query.t ->
+      ((module JSON_SERIALISABLE with type t = r) -> (unit -> w) -> z) ->
+      z option
+    = fun route controller path query return ->
+      match route with
+      | Return(module R) ->
+        (
+          if path = [] && Madge_query.is_empty query then
+            Some (return (module R) controller)
+          else
+            None
+        )
+      | Literal (str, route) ->
+        (
+          match path with
+          | comp :: path when comp = str -> match_ route controller path query return
+          | _ -> None
+        )
+      | Variable (rt, route) ->
+        (
+          match path with
+          | comp :: path ->
+            (
+              match rt.of_string comp with
+              | Some comp -> match_ route (fun () -> controller () comp) path query return
+              | _ -> None
+            )
+          | _ -> None
+        )
+      | Query (name, (module R), route) ->
+        (
+          match Madge_query.extract name query with
+          | Some (value, query) ->
+            (
+              match R.of_yojson value with
+              | Ok value -> match_ route (fun () -> controller () value) path query return
+              | _ -> None
+            )
+          | None -> None
+        )
+      | QueryOpt (name, (module R), route) ->
+        (
+          match Madge_query.extract name query with
+          | Some (value, query) ->
+            (
+              match R.of_yojson value with
+              | Ok value -> match_ route (fun () -> controller () (Some value)) path query return
+              | _ -> None
+            )
+          | None -> match_ route (fun () -> controller () None) path query return
+        )
+
+  let match_
+    : type a w r z. (a, w, r) t ->
+      a ->
+      Uri.t ->
+      ((module JSON_SERIALISABLE with type t = r) -> (unit -> w) -> z) ->
+      z option
+    = fun route controller uri return ->
+      let path = List.filter ((<>) "") (String.split_on_char '/' (Uri.path uri)) in
+      match_ route (fun () -> controller) path (Madge_query.from_uri uri) return
 end
 
 let uri : type a r. (a, Uri.t, r) Route.t -> a = fun route ->
--- src/madge/madge_server_new.ml
+++ src/madge/madge_server_new.ml
@@ -1,58 +1,11 @@
 open Nes
 include Madge_common_new
-open Route
 
-(* NOTE: The [controller] is in a thunk to avoid it being ran halfway as we find
-   its last argument. It is actually run at the end when all is green. *)
-let rec match_apply : type a r. (a, r Lwt.t, r) t -> (unit -> a) -> string list -> Madge_query.t -> string Lwt.t option = fun route controller path query ->
-  match route with
-  | Return(module R) ->
-    (
-      if path = [] && Madge_query.is_empty query then
-        Some (Lwt.map (Yojson.Safe.to_string % R.to_yojson) (controller ()))
-      else
-        None
-    )
-  | Literal (str, route) ->
-    (
-      match path with
-      | comp :: path when comp = str -> match_apply route controller path query
-      | _ -> None
-    )
-  | Variable (rt, route) ->
-    (
-      match path with
-      | comp :: path ->
-        (
-          match rt.of_string comp with
-          | Some comp -> match_apply route (fun () -> controller () comp) path query
-          | _ -> None
-        )
-      | _ -> None
-    )
-  | Query (name, (module R), route) ->
-    (
-      match Madge_query.extract name query with
-      | Some (value, query) ->
-        (
-          match R.of_yojson value with
-          | Ok value -> match_apply route (fun () -> controller () value) path query
-          | _ -> None
-        )
-      | None -> None
-    )
-  | QueryOpt (name, (module R), route) ->
-    (
-      match Madge_query.extract name query with
-      | Some (value, query) ->
-        (
-          match R.of_yojson value with
-          | Ok value -> match_apply route (fun () -> controller () (Some value)) path query
-          | _ -> None
-        )
-      | None -> match_apply route (fun () -> controller () None) path query
-    )
-
-let match_apply : type a r. (a, r Lwt.t, r) t -> a -> Uri.t -> string Lwt.t option = fun route controller uri ->
-  let path = List.filter ((<>) "") (String.split_on_char '/' (Uri.path uri)) in
-  match_apply route (fun () -> controller) path (Madge_query.from_uri uri)
+let match_apply
+  : type a r. (a, r Lwt.t, r) Route.t ->
+    a ->
+    Uri.t ->
+    string Lwt.t option
+  = fun route controller uri ->
+    Route.match_ route controller uri @@ fun (module R) thunk ->
+    Lwt.map (Yojson.Safe.to_string % R.to_yojson) (thunk ())
