9bf70fc843a680ea9447ef3584626a7333280d82
Niols <niols@niols.fr>
2025-01-28 14:15:49 +0100

    Remove useless modules
    They have been useless for a long time, I think.

--- src/common/dancelor_common.ml
+++ src/common/dancelor_common.ml
@@ -3,6 +3,4 @@ module Error = Error
 module Model = Dancelor_common_model
 module PageRouter = Dancelor_common_pageRouter
 module SCDDB = SCDDB
-module Serializer = Serializer
-module Unserializer = Unserializer
 module Database = Dancelor_common_database
