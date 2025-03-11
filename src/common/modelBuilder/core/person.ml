82d548d2589f268694025b85f9ba50dbded393eb
Niols <niols@niols.fr>
2025-03-11 18:30:54 +0100

    Use safe (de)serialisation of slugs in Madge

--- src/common/modelBuilder/core/person.ml
+++ src/common/modelBuilder/core/person.ml
@@ -13,5 +13,5 @@ let make ~name ?scddb_id () = make ~name ~scddb_id ()
 let name = name % Entry.value
 let scddb_id = scddb_id % Entry.value
 
-let trad_slug : t Slug.t = Slug.unsafe_of_string "traditional"
+let trad_slug : t Slug.t = Slug.check_string_exn "traditional"
 let is_trad c = Slug.equal' (Entry.slug c) trad_slug
