e5161e8f1899b0f7bcadac698b39879d1ad32809
Niols <niols@niols.fr>
2025-04-08 13:30:54 +0200

    Invalidate cache for all static items

--- src/server/static.ml
+++ src/server/static.ml
@@ -19,13 +19,13 @@ let index =
 
           (* Style *)
           meta ~a: [a_name "viewport"; a_content "width=device-width, initial-scale=1, maximum-scale=1"] ();
-          link ~rel: [`Stylesheet] ~href: "/fonts.css" ();
-          link ~rel: [`Stylesheet] ~href: "/style.css" ();
+          link ~rel: [`Stylesheet] ~href: ("/fonts.css?" ^ boot_time) ();
+          link ~rel: [`Stylesheet] ~href: ("/style.css?" ^ boot_time) ();
 
           (* Favicon *)
-          link ~rel: [`Icon] ~a: [a_mime_type "image/png"; a_sizes (Some [(32, 32)])] ~href: "/favicon-32x32.png" ();
-          link ~rel: [`Icon] ~a: [a_mime_type "image/png"; a_sizes (Some [(16, 16)])] ~href: "/favicon-16x16.png" ();
-          link ~rel: [`Manifest] ~href: "/site.webmanifest" ();
+          link ~rel: [`Icon] ~a: [a_mime_type "image/png"; a_sizes (Some [(32, 32)])] ~href: ("/favicon-32x32.png?" ^ boot_time) ();
+          link ~rel: [`Icon] ~a: [a_mime_type "image/png"; a_sizes (Some [(16, 16)])] ~href: ("/favicon-16x16.png?" ^ boot_time) ();
+          link ~rel: [`Manifest] ~href: ("/site.webmanifest?" ^ boot_time) ();
 
           (* Dancelor *)
           script ~a: [a_script_type `Javascript; a_src ("/client.js?" ^ boot_time)] (txt "");
