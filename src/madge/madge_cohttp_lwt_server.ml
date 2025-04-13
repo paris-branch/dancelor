f5c2cad4c45c364bb69a5a632402ae75e680a236
Niols <niols@niols.fr>
2025-04-13 17:09:24 +0200

    Allow an optional `post_process` argument to `match_apply`

--- src/madge/madge_cohttp_lwt_server.ml
+++ src/madge/madge_cohttp_lwt_server.ml
@@ -14,17 +14,30 @@ let cohttp_code_meth_to_meth = function
   | `Other _ ->
     assert false (* FIXME *)
 
+type response = {
+  status: Cohttp.Code.status_code;
+  headers: Cohttp.Header.t;
+  body: Yojson.Safe.t;
+}
+
 let match_apply
-  : type a r. (a, r Lwt.t, r) route ->
+  : type a r. ?post_process: (response -> response) ->
+  (a, r Lwt.t, r) route ->
   a ->
   request ->
   (unit -> (Cohttp.Response.t * Cohttp_lwt.Body.t) Lwt.t) option
-= fun route controller request ->
+= fun ?(post_process = Fun.id) route controller request ->
   match_ route controller request @@ fun (module R) promise ->
   Lwt.bind promise @@ fun value ->
-  let headers = Cohttp.Header.of_list [("Content-Type", "application/json")] in
-  let body = Yojson.Safe.to_string (R.to_yojson value) in
-  Cohttp_lwt_unix.Server.respond_string ~headers ~status: `OK ~body ()
+  let response = {
+    status = `OK;
+    headers = Cohttp.Header.of_list [("Content-Type", "application/json")];
+    body = R.to_yojson value;
+  }
+  in
+  let {status; headers; body} = post_process response in
+  let body = Yojson.Safe.to_string body in
+  Cohttp_lwt_unix.Server.respond_string ~status ~headers ~body ()
 
 exception Shortcut of (Cohttp.Response.t * Cohttp_lwt.Body.t)
 
