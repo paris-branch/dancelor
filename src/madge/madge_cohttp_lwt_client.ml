67fb2ee3b345138c45a924d41c20c0cad4b61db8
Niols <niols@niols.fr>
2025-04-16 22:12:36 +0200

    Better error reporting in Madge client

--- src/madge/madge_cohttp_lwt_client.ml
+++ src/madge/madge_cohttp_lwt_client.ml
@@ -11,16 +11,32 @@ let meth_to_cohttp_code_meth = function
   | TRACE -> `TRACE
   | CONNECT -> `CONNECT
 
+exception HttpError of {status: Cohttp.Code.status_code; message: string option}
+
+type error_response = {message: string option} [@@deriving yojson]
+
 let call : type a r. (a, r Lwt.t, r) route -> a = fun route ->
   process route @@ fun (module R) {meth; uri; body} ->
   let meth = meth_to_cohttp_code_meth meth in
   let body = Cohttp_lwt.Body.of_string body in
   let%lwt (response, body) = Cohttp_lwt_jsoo.Client.call meth uri ~body in
-  if Cohttp.(Code.(is_success (code_of_status (Response.status response)))) then
+  let%lwt body = Cohttp_lwt.Body.to_string body in
+  let body =
+    try
+      Yojson.Safe.from_string body
+    with
+      | Yojson.Json_error _fixme -> failwith "Madge_cohttp_lwt_client.call: body is not JSON"
+  in
+  let status = Cohttp.Response.status response in
+  if Cohttp.(Code.(is_success (code_of_status status))) then
     (
-      let%lwt body = Cohttp_lwt.Body.to_string body in
-      (* FIXME: error handling of JSON parsing and conversion from JSON *)
-      Lwt.return (Result.get_ok @@ R.of_yojson @@ Yojson.Safe.from_string body)
+      match R.of_yojson body with
+      | Error msg -> failwith @@ "Madge_cohttp_lwt_client.call: body cannot be unserialised: " ^ msg
+      | Ok body -> Lwt.return body
     )
   else
-    assert false (* FIXME: error handling *)
+    (
+      match error_response_of_yojson body with
+      | Error msg -> failwith @@ "Madge_cohttp_lwt_client.call: error body cannot be unserialised: " ^ msg
+      | Ok {message} -> raise @@ HttpError {status; message}
+    )
