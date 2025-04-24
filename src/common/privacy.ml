4d132b339d23280e89a0b2ee9cfa0d61efca69d2
Niols <niols@niols.fr>
2025-04-24 12:35:48 +0200

    Small documentation of Privacy module

--- src/common/privacy.ml
+++ src/common/privacy.ml
@@ -1,3 +1,8 @@
+(** {1 Privacy}
+
+    Levels of privacy are attached to entries in the database and influence who
+    gets to see them. *)
+
 type t =
   Private | Public
 [@@deriving show, yojson]
