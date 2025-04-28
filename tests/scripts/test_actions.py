272e679fd1a95e8055b1ef64ca99c65f1db32cb0
Niols <niols@niols.fr>
2025-04-28 23:09:53 +0200

    Fix Ly download test to follow correct content types
    Now that we give `application/x-lilypond` content type to the
    `content.ly`, Firefox just downloads it immediately, and that breaks the
    test. We handle testing this new situation properly.

--- tests/scripts/test_actions.py
+++ tests/scripts/test_actions.py
@@ -1,7 +1,10 @@
+import os
 import pytest
 import time
 import json
 import html
+import tempfile
+import shutil
 from urllib.parse import urlparse
 
 from selenium import webdriver
@@ -16,24 +19,37 @@ class TestVersionLyDownload():
   def setup_method(self, method):
     options = webdriver.FirefoxOptions()
     options.add_argument("--headless")
+    self.download_dir = tempfile.mkdtemp()
+    options.set_preference("browser.download.folderList", 2)
+    options.set_preference("browser.download.dir", self.download_dir)
+    options.set_preference("browser.download.useDownloadDir", True)
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "")
     self.driver = webdriver.Firefox(options=options)
     self.driver.implicitly_wait(10)
     self.vars = {}
 
   def teardown_method(self, method):
+    shutil.rmtree(self.download_dir)
     self.driver.quit()
+
+  def get_downloaded_file(self):
+    files = [f for f in os.listdir(self.download_dir) if os.path.isfile(os.path.join(self.download_dir, f))]
+    if len(files) == 0: raise FileNotFoundError("No files found in the download directory")
+    elif len(files) > 1: raise ValueError(f"Multiple files found in the download directory: {files}")
+    return os.path.join(self.download_dir, files[0])
 
   def test_versionLyDownload(self):
     self.driver.get("http://localhost:8080/version/tam-lin-niols")
     self.driver.find_element(By.XPATH, "//*[contains(text(), 'Actions')]").click()
-    self.driver.find_element(By.XPATH, "//*[contains(text(), 'LilyPond')]").click()
+    lilypond_button = self.driver.find_element(By.XPATH, "//*[contains(text(), 'LilyPond')]")
     ## NOTE: Check that the URL is what we expect.
-    assert(urlparse(self.driver.current_url).path == "/api/version/tam-lin-niols.ly")
-    ## NOTE: Check that the `content.ly` is indeed shown on the page. We cannot
-    ## check an equality because the page source might contain some HTML tags;
-    ## it might also come out as escaped HTML so we need to check that too.
+    assert(urlparse(lilypond_button.get_attribute('href')).path == "/api/version/tam-lin-niols.ly")
+    lilypond_button.click()
+    ## NOTE: Check that the `content.ly` is indeed what we downloaded.
     with open("tests/database/version/tam-lin-niols/content.ly") as content_file:
       expected_content = content_file.read().strip()
       assert(expected_content != "")
-    assert((expected_content in self.driver.page_source)
-           or (expected_content in html.unescape(self.driver.page_source)))
+    with open(self.get_downloaded_file()) as content_file:
+      actual_content = content_file.read().strip()
+      assert(actual_content != "")
+    assert(actual_content == expected_content)
