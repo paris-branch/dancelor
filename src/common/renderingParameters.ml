20455627c4e23fbedb04ce29fee34261444c6051
Niols <niols@niols.fr>
2025-05-05 18:58:32 +0200

    Add PDF metadata to rendering parameters

--- src/common/renderingParameters.ml
+++ src/common/renderingParameters.ml
@@ -9,11 +9,34 @@ type paper_size =
   | Custom of float * float * string (** width, height and unit *)
 [@@deriving eq, show {with_path = false}, yojson]
 
+type pdf_metadata = {
+  title: string; [@default ""]
+  subtitle: string; [@default ""]
+  composers: string list; [@default []] (* FIXME: difference between “author” and “composer”? *)
+  subjects: string list; [@default []] (** to be used for eg. kinds *)
+}
+[@@deriving fields, show, eq, yojson]
+
+let no_pdf_metadata = `Assoc [] |> pdf_metadata_of_yojson |> Result.get_ok
+
+let update_pdf_metadata ?title ?subtitle ?composers ?subjects pdf_metadata = {
+  title = Option.value title ~default: Fun.id pdf_metadata.title;
+  subtitle = Option.value subtitle ~default: Fun.id pdf_metadata.subtitle;
+  composers = Option.value composers ~default: Fun.id pdf_metadata.composers;
+  subjects = Option.value subjects ~default: Fun.id pdf_metadata.subjects;
+}
+
 type t = {
   paper_size: paper_size option; [@default None] [@key "paper-size"]
+  pdf_metadata: pdf_metadata; [@default no_pdf_metadata]
 }
 [@@deriving fields, yojson, eq, show]
 
 let none = `Assoc [] |> of_yojson |> Result.get_ok
+
+let update ?paper_size ?pdf_metadata params = {
+  paper_size = Option.value paper_size ~default: Fun.id params.paper_size;
+  pdf_metadata = Option.value pdf_metadata ~default: Fun.id params.pdf_metadata;
+}
 
 let paper_size' = Option.value ~default: (A 4) % paper_size
---
+++ src/server/controller/template/book/book_metadata.ly
@@ -0,0 +1,9 @@
+\header {
+ pdftitle = "%s"
+ pdfsubtitle = "%s"
+ pdfcomposer = "%s"
+ pdfsubject = "%s"
+
+ %% FIXME: does not work
+ %% pdfcreator = "Dancelor v1.2.3 + LilyPond v4.5.6"
+}
