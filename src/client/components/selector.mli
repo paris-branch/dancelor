ad0b5c9436f749688b4bcdecb6bd1b1d326dea3c
Niols <niols@niols.fr>
2025-08-14 13:42:03 +0000

    Add a variant of the selector that is allowed no to contain a model

--- src/client/components/selector.mli
+++ src/client/components/selector.mli
@@ -46,3 +46,23 @@ val prepare :
 (** Variant of {!make} that only prepares the component. It must still be
     {!Component.initialise}d. This is used for composition with eg.
     {!ComponentList}. *)
+
+val prepare_opt :
+  label: string ->
+  search: (Slice.t -> string -> (int * 'model Entry.t list, string) Result.t Lwt.t) ->
+  unserialise: ('model Entry.Id.t -> 'model Entry.t option Lwt.t) ->
+  make_result:
+  (?classes: string list ->
+  ?action: Utils.ResultRow.action ->
+  ?prefix: Utils.ResultRow.cell list ->
+  ?suffix: Utils.ResultRow.cell list ->
+  'model Entry.t ->
+  Utils.ResultRow.t) ->
+  ?make_more_results:
+  ('model Entry.t ->
+  Utils.ResultRow.t list S.t) ->
+  model_name: string ->
+  create_dialog_content: (('model Entry.t, 'any) Editor.mode -> Page.t Lwt.t) ->
+  unit ->
+  ('model Entry.t option, 'model Entry.Id.t option) Component.s
+(** Variant of {!prepare} that is allowed to return no model. *)
