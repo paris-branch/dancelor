6a1da88a68533361ae383acb7a90355ab1b0f6a3
Niols <niols@niols.fr>
2025-08-28 15:56:13 +0200

    Introduce a `Pair` module to Nes

---
+++ src/nes/nesPair.ml
@@ -0,0 +1,14 @@
+let fst (x, _) = x
+let snd (_, y) = y
+
+let set_fst x (_, y) = (x, y)
+let set_snd y (x, _) = (x, y)
+
+let map_fst f (x, y) = (f x, y)
+let map_snd g (x, y) = (x, g y)
+let map_both f g (x, y) = (f x, g y)
+
+let map2_both f g (x1, y1) (x2, y2) = (f x1 x2, g y1 y2)
+
+let cons x y = (x, y)
+let snoc y x = (x, y)
