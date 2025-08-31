1fff8013d2d7d0012d44378f22f5f9b9f5ee5a16
Niols <niols@niols.fr>
2025-09-01 00:53:34 +0200

    Only show logs for version SVG in version editor

--- src/client/components/versionSvg.ml
+++ src/client/components/versionSvg.ml
@@ -3,7 +3,21 @@ open Common
 
 open Html
 
-let make_gen status_signal =
+let make_gen ?(show_logs = false) status_signal =
+  let on_succeeded href = [
+    object_
+      ~a: [
+        (* Tune previews are generated on A4 paper -- 21cm wide -- with a left margin
+           of 1cm for page numbers -- that is 5% of the whole page. Since we want page
+           numbers to actually appear in the margin on the website, we make the image
+           bigger but remove those 5% of margin on the left. *)
+        a_style "margin-left: -5%; width: 105%;";
+        a_mime_type "image/svg+xml";
+        a_data href;
+      ]
+      []
+  ]
+  in
   R.div
     ~a: [
       (* On mobile, space gets parse, so we compensate the container's padding
@@ -11,26 +25,12 @@ let make_gen status_signal =
          width (effectively hiding the bar numbers; see also below). *)
       a_class ["mx-n2"; "mx-sm-0"];
     ] @@
-    Job.show_live_status
+    (if show_logs then Job.show_live_status else Job.show_placeholder)
+      ~on_succeeded
       status_signal
-      ~on_succeeded: (fun href ->
-        [
-          object_
-            ~a: [
-              (* Tune previews are generated on A4 paper -- 21cm wide -- with a left margin
-                 of 1cm for page numbers -- that is 5% of the whole page. Since we want page
-                 numbers to actually appear in the margin on the website, we make the image
-                 bigger but remove those 5% of margin on the left. *)
-              a_style "margin-left: -5%; width: 105%;";
-              a_mime_type "image/svg+xml";
-              a_data href;
-            ]
-            []
-        ]
-      )
 
-let make ?(params = Model.VersionParameters.none) version =
-  make_gen @@
+let make ?show_logs ?(params = Model.VersionParameters.none) version =
+  make_gen ?show_logs @@
   Job.status_signal_from_promise @@
   let%lwt slug = Model.Version.slug' version in
   Job.run
@@ -40,8 +40,8 @@ let make ?(params = Model.VersionParameters.none) version =
     params
     RenderingParameters.none
 
-let make_preview ?(params = Model.VersionParameters.none) version =
-  make_gen @@
+let make_preview ?show_logs ?(params = Model.VersionParameters.none) version =
+  make_gen ?show_logs @@
   Job.status_signal_from_promise @@
   Job.run
     Entry.Slug.(add_suffix (of_string "preview") ".svg")
