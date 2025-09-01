d55e67ba8d2582b9b7ba8aaa4e02a641b6cb0884
Niols <niols@niols.fr>
2025-09-01 15:08:57 +0200

    Get rid of dummy entries
    Closes #457
    Closes #620

--- src/common/entry/entry.ml
+++ src/common/entry/entry.ml
@@ -28,28 +28,18 @@ let update_meta ?status ?privacy ?created_at ?modified_at meta = {
 }
 
 type 'a t =
-  | Full of 'a Id.t * meta * 'a
-  | Dummy of 'a
-[@@deriving show {with_path = false}]
-
-let is_dummy = function Dummy _ -> true | Full _ -> false
+  {id: 'a Id.t; meta: meta; value: 'a}
+[@@deriving show {with_path = false}, fields]
 
 let make' ~id ?meta value =
   let meta = Option.value ~default: (make_meta ()) meta in
-  Full (id, meta, value)
+    {id; meta; value}
 
 let make ~id ?status ?privacy ?created_at ?modified_at value =
   make' ~id ~meta: (make_meta ?status ?privacy ?created_at ?modified_at ()) value
 
-let make_dummy value = Dummy value
-
-exception UsedGetterOnDummy
-
-let id = function Full (id, _, _) -> id | _ -> raise UsedGetterOnDummy
 let id' e = Id.unsafe_coerce (id e)
 let id_as_string e = Id.to_string (id e)
-let meta = function Full (_, meta, _) -> meta | _ -> raise UsedGetterOnDummy
-let value = function Full (_, _, value) | Dummy value -> value
 
 let status' e = status @@ meta e
 let privacy' e = privacy @@ meta e
@@ -59,16 +49,12 @@ let equal _ = equal'
 let compare' e f = Id.compare' (id e) (id f)
 let compare _ = compare'
 
-let to_yojson value_to_yojson = function
-  | Full (id, meta, value) ->
-    Json.add_field "id" (Id.to_yojson value_to_yojson id) @@
-      Json.merge_assoc (value_to_yojson value) (meta_to_yojson meta)
-  | Dummy v ->
-    value_to_yojson v
+let to_yojson value_to_yojson {id; meta; value} =
+  Json.add_field "id" (Id.to_yojson value_to_yojson id) @@
+    Json.merge_assoc (value_to_yojson value) (meta_to_yojson meta)
 
-let to_yojson' value_to_yojson = function
-  | Full (_, meta, value) -> Json.merge_assoc (value_to_yojson value) (meta_to_yojson meta)
-  | Dummy _ -> raise UsedGetterOnDummy
+let to_yojson' value_to_yojson {meta; value; _} =
+  Json.merge_assoc (value_to_yojson value) (meta_to_yojson meta)
 
 let of_yojson' id value_of_yojson json =
   let (status, json) = Option.value (Json.extract_field_opt "status" json) ~default: (Status.(to_yojson bot), json) in
@@ -78,14 +64,14 @@ let of_yojson' id value_of_yojson json =
   let meta_json = `Assoc [("status", status); ("privacy", privacy); ("created-at", created_at); ("modified-at", modified_at)] in
   Result.bind (meta_of_yojson meta_json) @@ fun meta ->
   Result.bind (value_of_yojson value_json) @@ fun value ->
-  Ok (Full (id, meta, value))
+  Ok {id; meta; value}
 
 let of_yojson value_of_yojson json =
   match Json.extract_field_opt "id" json with
   | Some (id, json) ->
     Result.bind (Id.of_yojson value_of_yojson id) @@ fun id ->
     of_yojson' id value_of_yojson json
-  | None -> Result.map (fun x -> Dummy x) (value_of_yojson json)
+  | None -> Result.error "missing id"
 
 module J (A : Madge.JSONABLE) : Madge.JSONABLE with type t = A.t t = struct
   type nonrec t = A.t t
@@ -93,7 +79,5 @@ module J (A : Madge.JSONABLE) : Madge.JSONABLE with type t = A.t t = struct
   let of_yojson = of_yojson A.of_yojson
 end
 
-let unsafe_set_value entry value =
-  match entry with
-  | Full (id, meta, _) -> Full (Id.unsafe_coerce id, meta, value)
-  | Dummy _ -> Dummy value
+let unsafe_set_value {id; meta; _} value =
+  {id = Id.unsafe_coerce id; meta; value}
--- src/common/entry/entry.mli
+++ src/common/entry/entry.mli
@@ -4,12 +4,7 @@ module Id = Id
 module Slug = ESlug
 
 type 'a t [@@deriving show]
-(** A database entry. Can either be a full database entry or a dummy. Dummies
-    should be avoided, and the getters will fail when dealing with them, but can
-    be used in places that require dealing with a database table. *)
-
-val is_dummy : 'a t -> bool
-(** Test whether the given entry is a dummy. *)
+(** A database entry.*)
 
 (** {2 Builders} *)
 
@@ -46,26 +41,13 @@ val make' :
   'a ->
   'a t
 
-val make_dummy : 'a -> 'a t
-
 (** {2 Getters} *)
 
-exception UsedGetterOnDummy
-(** Exception raised when trying to access field of a dummy entry. *)
-
 val id : 'a t -> 'a Id.t
-(** @raise UsedGetterOnDummy if the entry is a dummy. *)
-
 val id' : 'a t -> 'a t Id.t
-(** @raise UsedGetterOnDummy if the entry is a dummy. *)
-
 val id_as_string : 'a t -> string
-(** @raise UsedGetterOnDummy if the entry is a dummy. *)
-
 val value : 'a t -> 'a
-
 val meta : 'a t -> meta
-(** @raise UsedGetterOnDummy if the entry is a dummy. *)
 
 val status : meta -> Status.t
 val status' : 'a t -> Status.t
@@ -80,20 +62,14 @@ val modified_at : meta -> Datetime.t
 (** {2 Comparison} *)
 
 val equal : ('a -> 'a -> bool) -> 'a t -> 'a t -> bool
-(** Comparison for ppx_deriving_yojson. The first argument is ignored. Use {!equal'}.
-
-    @raise UsedGetterOnDummy if the entry is a dummy. *)
+(** Comparison for ppx_deriving_yojson. The first argument is ignored. Use {!equal'}. *)
 
 val equal' : 'a t -> 'a t -> bool
-(** @raise UsedGetterOnDummy if the entry is a dummy. *)
 
 val compare : ('a -> 'a -> int) -> 'a t -> 'a t -> int
-(** Comparison for ppx_deriving_yojson. The first argument is ignored. Use {!compare'}.
-
-    @raise UsedGetterOnDummy if the entry is a dummy. *)
+(** Comparison for ppx_deriving_yojson. The first argument is ignored. Use {!compare'}. *)
 
 val compare' : 'a t -> 'a t -> int
-(** @raise UsedGetterOnDummy if the entry is a dummy. *)
 
 (** {2 Serialisation} *)
 
@@ -101,9 +77,7 @@ val to_yojson : ('a -> Yojson.Safe.t) -> 'a t -> Yojson.Safe.t
 val of_yojson : (Yojson.Safe.t -> ('a, string) result) -> Yojson.Safe.t -> ('a t, string) result
 
 val to_yojson' : ('a -> Yojson.Safe.t) -> 'a t -> Yojson.Safe.t
-(** Variant of {!to_yojson} that doesn't serialise the id.
-
-    @raise UsedGetterOnDummy if the entry is a dummy. *)
+(** Variant of {!to_yojson} that doesn't serialise the id. *)
 
 val of_yojson' : 'a Id.t -> (Yojson.Safe.t -> ('a, string) result) -> Yojson.Safe.t -> ('a t, string) result
 (** Variant of {!of_yojson} that expects the id as an argument instead of in
