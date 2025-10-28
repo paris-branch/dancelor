1e998c178da6238a775e53de2bcab27b9e1cc130
Niols <niols@niols.fr>
2025-10-28 09:11:35 +0100

    XPATH?

--- tests/scripts/test_contextLinks.py
+++ tests/scripts/test_contextLinks.py
@@ -50,7 +50,9 @@ class TestContextLinks():
     ## check that the resulting URL contains the right context.
     self.driver.get("http://localhost:8080/book/0fi3-1iot-6tbq")
     time.sleep(1) # give a second to avoid clicking on placeholder
-    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(2) > td:nth-child(1)").click()
+    self.driver.find_element(By.XPATH, "((//tr)[2]/td)[1]").click()
+    time.sleep(10)
+    print(self.driver.current_url)
     self.wait.until(EC.url_to_be("http://localhost:8080/set/ului-yd9x-o35w?context=%5B%22InBook%22%2C%220fi3-1iot-6tbq%22%2C1%5D"))
 
   def test_sideArrowGoesToNeighbour(self):
