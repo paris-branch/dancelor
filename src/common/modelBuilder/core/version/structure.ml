b9258e1d765ccf24d4271307a3a4a4fd5dcb96b8
Niols <niols@niols.fr>
2025-11-23 21:53:21 +0100

    Move helpers from `ModelToRenderer` into `Core.Version.{Structure,Voices}`

--- src/common/modelBuilder/core/version/structure.ml
+++ src/common/modelBuilder/core/version/structure.ml
@@ -10,3 +10,77 @@ let to_yojson s = `String (NEString.to_string @@ to_string s)
 let of_yojson = function
   | `String s -> Option.to_result ~none: "not a valid structure" (Option.bind (NEString.of_string s) of_string)
   | _ -> Error "not a string"
+
+type folded_item = Part of Part_name.t | Repeat of int * folded
+and folded = folded_item list
+
+(** A few helpers *)
+let a = Part (Part_name.of_char_exn 'A')
+let b = Part (Part_name.of_char_exn 'B')
+let c = Part (Part_name.of_char_exn 'C')
+let d = Part (Part_name.of_char_exn 'D')
+let e = Part (Part_name.of_char_exn 'E')
+let f = Part (Part_name.of_char_exn 'F')
+
+let part n = Part n
+let repeat n x = Repeat (n, x)
+
+let rec first_part_exn structure =
+  match List.hd_opt structure with
+  | None -> failwith "first_part_exn"
+  | Some Part a -> a
+  | Some Repeat (_, structure) -> first_part_exn structure
+
+let first_part structure =
+  match List.hd_opt structure with
+  | None -> None
+  | Some Part a -> Some a
+  | Some Repeat (_, structure) -> Some (first_part_exn structure)
+
+let rec last_part_exn structure =
+  match List.ft_opt structure with
+  | None -> failwith "last_part_exn"
+  | Some Part a -> a
+  | Some Repeat (_, structure) -> last_part_exn structure
+
+let last_part structure =
+  match List.ft_opt structure with
+  | None -> None
+  | Some Part a -> Some a
+  | Some Repeat (_, structure) -> Some (last_part_exn structure)
+
+(** A general algorithm would be great, but for now this will do. *)
+let best_fold_for structure =
+  match NEString.to_string @@ to_string structure with
+  | "A" -> some [a]
+  | "B" -> some [b]
+  | "C" -> some [c]
+  | "D" -> some [d]
+  | "AB" -> some [a; b]
+  | "AAB" -> some [repeat 2 [a]; b]
+  | "ABB" -> some [a; repeat 2 [b]]
+  | "ABC" -> some [a; b; c]
+  | "AAAA" -> some [repeat 4 [a]]
+  | "AAAB" -> some [repeat 3 [a]; b]
+  | "AABA" -> some [repeat 2 [a]; b; a]
+  | "AABB" -> some [repeat 2 [a]; repeat 2 [b]]
+  | "ABAA" -> some [a; b; repeat 2 [a]]
+  | "ABAB" -> some [repeat 2 [a; b]]
+  | "ABBA" -> some [a; repeat 2 [b]; a]
+  | "ABBB" -> some [a; repeat 3 [b]]
+  | "AABC" -> some [repeat 2 [a]; b; c]
+  | "ABCD" -> some [a; b; c; d]
+  | "AABBB" -> some [repeat 2 [a]; repeat 3 [b]]
+  | "ABABA" -> some [repeat 2 [a; b]; a]
+  | "ABABB" -> some [repeat 2 [a; b]; b]
+  | "ABBAB" -> some [a; repeat 2 [b]; a; b]
+  | "ABBCC" -> some [a; repeat 2 [b]; repeat 2 [c]]
+  | "ABABC" -> some [repeat 2 [a; b]; c]
+  | "ABCDE" -> some [a; b; c; d; e]
+  | "ABABAB" -> some [repeat 3 [a; b]]
+  | "AABBAB" -> some [repeat 2 [a]; repeat 2 [b]; a; b]
+  | "AABBCC" -> some [repeat 2 [a]; repeat 2 [b]; repeat 2 [c]]
+  | "ABCDEF" -> some [a; b; c; d; e; f]
+  | "AABBCCDD" -> some [repeat 2 [a]; repeat 2 [b]; repeat 2 [c]; repeat 2 [d]]
+  | "AABCDDEF" -> some [repeat 2 [a]; b; c; repeat 2 [d]; e; f]
+  | _ -> None
--- src/common/modelBuilder/signature/version.ml
+++ src/common/modelBuilder/signature/version.ml
@@ -22,10 +22,34 @@ module type S = sig
   end
 
   module Structure : sig
-    type t = (*  *Core.Version.Structure.t = *) Part_name.t NEList.t
+    type t = (* Core.Version.Structure.t = *) Part_name.t NEList.t
 
     val to_string : t -> NEString.t
     val of_string : NEString.t -> t option
+
+    type folded_item = Core.Version.Structure.folded_item = Part of Part_name.t | Repeat of int * folded
+    and folded = folded_item list
+
+    val part : Part_name.t -> folded_item
+    val repeat : int -> folded -> folded_item
+
+    val first_part : folded -> Part_name.t option
+    (** Find the first part in a structure, regarless of whether it appears in a
+        repeat. *)
+
+    val first_part_exn : folded -> Part_name.t
+    (** Like {!first_part} but raises [Failure] if there is no first part. *)
+
+    val last_part : folded -> Part_name.t option
+    (** Find the last part in a structure, regarless of whether it appears in a
+        repeat. *)
+
+    val last_part_exn : folded -> Part_name.t
+    (** Like {!last_part} but raises [Failure] if there is no last part. *)
+
+    val best_fold_for : t -> folded option
+    (** Given a (normal, flat) structure, compute the best possible fold for it,
+        or return [None] if there is no good one. *)
   end
 
   module Voices : sig
@@ -34,6 +58,26 @@ module type S = sig
       chords: string; (** the chords of that part; they will be interpreted in LilyPond's [\chordmode] *)
     }
     [@@deriving fields]
+
+    val empty : t
+
+    val space : t
+    (** A space in both melody and chords. *)
+
+    val section_break : t
+    (** [\section\break] in the melody; space in the chords. *)
+
+    val fine : t
+    (** [\fine] in the melody; nothing in the chords. *)
+
+    val mark : Part_name.t -> t
+    (** A part name mark in the melody; nothing in the chords. *)
+
+    val concat : t -> t -> t
+    (** Concatenates melodies together and chords together in both voices. *)
+
+    val concat_l : t list -> t
+    (** Like {!concat} but on a list of voices. *)
   end
 
   module Content : sig
