670eeca443c5525f8048c0ea564156454ab8b5f0
Niols <niols@niols.fr>
2026-02-09 14:41:59 +0100

    Expose number of open file descriptors as a Prometheus metric

--- src/server/metrics.ml
+++ src/server/metrics.ml
@@ -7,3 +7,15 @@ let http_requests_total =
     "http_requests_total"
 
 let increment_http_requests_total () = Prometheus.Counter.inc_one http_requests_total
+
+let open_file_descriptors =
+  Prometheus.Gauge.v
+    ~namespace
+    ~help: "Number of open file descriptors"
+    "open_file_descriptors"
+
+let () =
+  Prometheus.CollectorRegistry.(register_pre_collect default) (fun () ->
+    Prometheus.Gauge.set open_file_descriptors @@
+      float_of_int (try Array.length (Sys.readdir "/proc/self/fd") - 2 with Sys_error _ -> 0)
+  )
