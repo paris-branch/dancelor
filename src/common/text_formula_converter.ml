f6bcc770ae0cabdf3a6c69871ef388bcb3d8f098
Niols <niols@niols.fr>
2026-03-10 17:10:50 +0100

    Text formula converter: separate lifters from other cases
    This commit also changes the semantics of the conversion from text
    formula to formula, because it will now merge silently at all levels,
    without needed to explicitly call `merge` and specify the merging strategy.

--- src/common/text_formula_converter.ml
+++ src/common/text_formula_converter.ml
@@ -8,36 +8,97 @@ type 'p case = {
   predicate_to_text_formula: 'p -> Type.t option; (** Given a ['p]redicate, produce a text formula if the case matches, or [None]. *)
 }
 
-type 'p t = {
+type inline = Inline | No_inline
+(* FIXME: There probably needs to be variants handling Raw differently. *)
+
+type _ lifter =
+  Lifter :
+    {
+      name: string;
+      lift: 'q Formula.t -> 'p;
+      unlift: 'p -> 'q Formula.t option;
+      converter: 'q t;
+      inline: inline;
+    } ->
+      'p lifter
+
+and 'p t = {
   debug_name: string;
   debug_print: Format.formatter -> 'p -> unit;
   raw: string -> ('p Formula.t, string) result; (** Given a string outside predicates, produce either a ['p Formula.t] or an [Error]. *)
+  lifters: 'p lifter list;
   other_cases: 'p case list;
 }
-[@@deriving fields]
 
-let make ~debug_name ~debug_print ~raw other_cases =
-  {debug_name; debug_print; raw; other_cases}
+let raw converter = converter.raw
+let debug_name converter = converter.debug_name
+let debug_print converter = converter.debug_print
 
-let cases converter =
+let lifter ~name ?(inline = No_inline) (lift, unlift) converter =
+  Lifter {name; lift; unlift; converter; inline}
+
+let make ~debug_name ~debug_print ~raw ?(lifters = []) other_cases =
+  {debug_name; debug_print; raw; lifters; other_cases}
+
+let rec cases
+  : type p. p t -> p case list
+= fun converter ->
   let raw_case = {
     text_predicate_to_formula = (function Type.Raw s -> Some (converter.raw s) | _ -> None);
     predicate_to_text_formula = const None;
   }
   in
-  raw_case :: converter.other_cases
+  let lifter_cases =
+    List.concat_map
+      (fun (Lifter {name; lift; unlift; converter; inline}) ->
+        {
+          text_predicate_to_formula = (function
+            | Type.Unary (name', tp) when name = name' ->
+              Some (Result.map Formula.pred ((Result.map lift % text_formula_to_formula converter) tp))
+            | _ -> None
+          );
+          predicate_to_text_formula = (fun predicate ->
+            match unlift predicate with
+            | None -> None
+            | Some qf ->
+              let qf = formula_to_text_formula converter qf in
+              match inline with
+              | No_inline -> Some (Type.unary' name qf)
+              | Inline -> Some qf
+          );
+        } ::
+        match inline with
+        | No_inline -> []
+        | Inline ->
+          List.map
+            (fun case ->
+              {
+                text_predicate_to_formula = Option.map (Result.map_both ~ok: (Formula.pred % lift) ~error: (spf "lifted %s: %s" name)) % case.text_predicate_to_formula;
+                predicate_to_text_formula = const None;
+              }
+            )
+            (cases converter)
+      )
+      converter.lifters
+  in
+  [raw_case] @ lifter_cases @ converter.other_cases
 
-let text_predicate_to_formula converter text_predicate =
-  Option.value
-    ~default: (kaspf error "No “%s” converter for predicate: %a." converter.debug_name Printer.pp_predicate text_predicate)
-    (List.find_map (fun case -> case.text_predicate_to_formula text_predicate) (cases converter))
+and text_predicate_to_formula : type p. p t -> Text_formula_type.predicate -> (p Formula.t, string) Result.t = fun converter text_predicate ->
+  match List.filter_map (fun case -> case.text_predicate_to_formula text_predicate) (cases converter) with
+  | [] -> Error (aspf "No “%s” converter for predicate: %a." converter.debug_name Printer.pp_predicate text_predicate)
+  | results ->
+    let (oks, errors) = List.partition_map (function Ok x -> Left x | Error y -> Right y) results in
+    match oks with
+    | [] -> Error (String.concat "\n" errors)
+    | results -> Ok (Formula.or_l results)
 
-let text_formula_to_formula converter = Formula.convert_res (text_predicate_to_formula converter)
+and text_formula_to_formula : type p. p t -> Text_formula_type.t -> (p Formula.t, string) Result.t = fun converter ->
+  Formula.convert_res (text_predicate_to_formula converter)
 
-let predicate_to_text_formula converter predicate =
+and predicate_to_text_formula : type p. p t -> p -> Text_formula_type.t option = fun converter predicate ->
   List.find_map (fun case -> case.predicate_to_text_formula predicate) (cases converter)
 
-let formula_to_text_formula converter formula =
+and formula_to_text_formula : type p. p t -> p Formula.t -> Text_formula_type.t = fun converter formula ->
   match Formula.convert_opt (predicate_to_text_formula converter) formula with
   | None -> failwith @@ spf "Text_formula_converter.of_formula: incomplete formula converter “%s”" converter.debug_name
   | Some tf -> tf
@@ -46,6 +107,19 @@ let map ?(error = Fun.id) f converter = {
   debug_name = "<opaque map>";
   debug_print = (fun fmt _ -> fpf fmt "<opaque map>");
   raw = Result.map_both ~ok: (Formula.pred % f) ~error % converter.raw;
+  lifters =
+  List.map
+    (function
+      | Lifter {name; lift; unlift = _; converter; inline} ->
+        Lifter {
+          name;
+          lift = f % Formula.pred % lift;
+          unlift = const None;
+          converter;
+          inline;
+        }
+    )
+    converter.lifters;
   other_cases =
   List.map
     (fun case ->
@@ -72,7 +146,8 @@ let merge ?(tiebreaker = Both) converter1 converter2 =
   {
     debug_name = spf "%s+%s" converter1.debug_name converter2.debug_name;
     debug_print = (fun fmt _ -> fpf fmt "<opaque merge>");
-    raw = (fun str -> merge_results (converter1.raw str) (converter2.raw str));
+    raw = (fun _str -> Error "there is no raw in a merge");
+    lifters = [];
     other_cases = [
       {
         text_predicate_to_formula = (fun text_predicate ->
