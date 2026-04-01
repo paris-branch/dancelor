7c645840b1e58e4cf09d0e02ce5ca20b675dba60
Niols <niols@niols.fr>
2026-04-01 10:53:08 +0200

    Fix `Lwt.backtrace_bind` needing filename and line number

--- src/nes/nesOlwt.ml
+++ src/nes/nesOlwt.ml
@@ -6,7 +6,11 @@ end
 type 'a t = 'a option Lwt.t
 
 let bind e f =
+  (* FIXME: Since recent versions, [Lwt.backtrace_bind] wants the filename and
+     the line number, presumably to track things better in backtraces. *)
   Lwt.backtrace_bind
+    "NesOlwt FIXME"
+    0
     (fun exn -> try Reraise.reraise exn with exn -> exn)
     e
     (function
