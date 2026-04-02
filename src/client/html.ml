c3bd3be434cbf85bbebc3717139b9166eceb6547
Niols <niols@niols.fr>
2026-04-03 00:33:01 +0200

    Make `Html.S.from_*` such that they work on functional values
    Fixes `Invalid_argument("compare: functional value")` in version viewer
    
    Closes #896

--- src/client/html.ml
+++ src/client/html.ml
@@ -25,7 +25,9 @@ module S = struct
          that as placeholder, and this way we avoid weird flickers. *)
       Stdlib.Option.value (get_available_1 container) ~default: placeholder
     in
-    let result, send_result = create placeholder in
+    (* No equality: trgger a change every time we call [send_result]. This
+       ensures we can put what we want in the signal, such as functions. *)
+    let result, send_result = create ~eq: (const2 false) placeholder in
     Lwt.async (fun () ->
       (* Iter over the container; when it is done, stop the signal as well. *)
       Lwt.finalize
