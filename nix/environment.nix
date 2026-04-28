c0896d600b2840a4a737d31d4490ab5673bc3e9c
Niols <niols@niols.fr>
2026-04-28 03:09:39 +0200

    Keep schema file formatted
    Now that it isn't dumped anymore.

--- nix/environment.nix
+++ nix/environment.nix
@@ -46,10 +46,7 @@
           name = "sqlfluff";
           entry = "${../scripts/sqlfluff-fix} --sqlfluff ${pkgs.sqlfluff}/bin/sqlfluff";
           files = "\\.sql$";
-          excludes = [
-            "^src/server/database/schema\\.sql$"
-            "^tests/database\\.sql$"
-          ];
+          excludes = [ "^tests/database\\.sql$" ];
           language = "system";
           pass_filenames = true;
         };
