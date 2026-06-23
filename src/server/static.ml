a32e3b3acd5b8fbd38447f11056b38540b8100f9
Niols <niols@niols.fr>
2026-06-23 15:30:35 +0200

    Use hashes instead of the boot time to force reload assets
    This includes in particular `client.js`, so we avoid making people
    reload the client at every reboot of Dancelor (at least once a day).
    
    Closes #985

--- src/server/static.ml
+++ src/server/static.ml
@@ -7,50 +7,68 @@ module Log = (val Logs.src_log @@ Logs.Src.create "server.static": Logs.LOG)
 
 let base_url = Uri.of_string "https://dancelor.org"
 
-let serve_index path =
-  (* NOTE: We use the boot time of Dancelor to invalidate caching of `client.js`.
-     This means that clients will have to re-download every time Dancelor starts,
-     but that isn't so often, and most of the time it is because of a change where
-     we would want to invalidate things anyway. *)
-  let index =
-    let boot_time = Datetime.to_string Environment.boot_time in
-    Format.asprintf "%a" (pp ()) @@
-      html
-        ~a: [a_lang "en"]
-        (
-          head (title (txt "Dancelor")) [
-            meta ~a: [a_charset "utf-8"] ();
-            link ~rel: [`Canonical] ~href: (Uri.to_string @@ Uri.with_path base_url path) ();
+let static_file_path path = Filename.concat (Config.get ()).share path
 
-            (* Style *)
-            meta ~a: [a_name "viewport"; a_content "width=device-width, initial-scale=1, maximum-scale=1"] ();
-            meta ~a: [a_name "description"; a_content "Dancelor — A community-edited database of Scottish country dance music. Search for tunes, assemble sets and books, and export to PDF, ready to print and bring to the dance."] ();
-            link ~rel: [`Stylesheet] ~href: ("/fonts.css?" ^ boot_time) ();
-            link ~rel: [`Stylesheet] ~href: ("/style.css?" ^ boot_time) ();
+let serve_static_file path =
+  let full_path = static_file_path path in
+  if Sys.file_exists full_path && not (Sys.is_directory full_path) then
+    Some (fun () ->
+      (* Keep static files in cache for 30 days. *)
+      let headers = Cohttp.Header.init_with "Cache-Control" "max-age=2592000" in
+      Cohttp_lwt_unix.Server.respond_file ~headers ~fname: full_path ()
+    )
+  else
+    None
 
-            (* Favicon *)
-            link ~rel: [`Icon] ~a: [a_mime_type "image/png"; a_sizes (Some [(32, 32)])] ~href: ("/favicon-32x32.png?" ^ boot_time) ();
-            link ~rel: [`Icon] ~a: [a_mime_type "image/png"; a_sizes (Some [(16, 16)])] ~href: ("/favicon-16x16.png?" ^ boot_time) ();
-            link ~rel: [`Manifest] ~href: ("/site.webmanifest?" ^ boot_time) ();
+let href_with_hash path =
+  spf "/%s?%s" path @@ Digest.BLAKE256.(to_hex % file) @@ static_file_path path
 
-            (* Ahrefs ownership proof and analytics *)
-            meta ~a: [a_name "ahrefs-site-verification"; a_content "4c418f04303adf3925d1c6bdef51b71cab26cd179ab478ecd1996cf71ce52ba4"] ();
-            script ~a: [a_src "https://analytics.ahrefs.com/analytics.js"; a_user_data "key" "HmcihempNgdCWYAHGvIYsg"; a_async ()] (txt "");
-
-            (* Dancelor *)
-            script ~a: [a_script_type `Javascript; a_src ("/client.js?" ^ boot_time)] (txt "");
-          ]
-        )
-        (
-          body
-            ~a: [a_class ["placeholder-glow"]]
-            [
-              script ~a: [a_src "/bootstrap.bundle.min.js"] (txt "");
+let serve_index =
+  (* NOTE: We add the hash of static files to their URL to invalidate
+     cache (in particular `client.js`). Otherwise, we cache them for
+     as long as possible. *)
+  let fonts_css_href = Lazy.from_fun @@ fun () -> href_with_hash "fonts.css" in
+  let style_css_href = Lazy.from_fun @@ fun () -> href_with_hash "style.css" in
+  let favicon_16x16_png_href = Lazy.from_fun @@ fun () -> href_with_hash "favicon-16x16.png" in
+  let favicon_32x32_png_href = Lazy.from_fun @@ fun () -> href_with_hash "favicon-32x32.png" in
+  let site_webmanifest_href = Lazy.from_fun @@ fun () -> href_with_hash "site.webmanifest" in
+  let client_js_href = Lazy.from_fun @@ fun () -> href_with_hash "client.js" in
+  let bootstrap_bundle_min_js_href = Lazy.from_fun @@ fun () -> href_with_hash "bootstrap.bundle.min.js" in
+  fun path ->
+    let index =
+      Format.asprintf "%a" (pp ()) @@
+        html
+          ~a: [a_lang "en"]
+          (
+            head (title (txt "Dancelor")) [
+              meta ~a: [a_charset "utf-8"] ();
+              link ~rel: [`Canonical] ~href: (Uri.to_string @@ Uri.with_path base_url path) ();
+              (* Style *)
+              meta ~a: [a_name "viewport"; a_content "width=device-width, initial-scale=1, maximum-scale=1"] ();
+              meta ~a: [a_name "description"; a_content "Dancelor — A community-edited database of Scottish country dance music. Search for tunes, assemble sets and books, and export to PDF, ready to print and bring to the dance."] ();
+              link ~rel: [`Stylesheet] ~href: (Lazy.force fonts_css_href) ();
+              link ~rel: [`Stylesheet] ~href: (Lazy.force style_css_href) ();
+              (* Favicon *)
+              link ~rel: [`Icon] ~a: [a_mime_type "image/png"; a_sizes (Some [(32, 32)])] ~href: (Lazy.force favicon_32x32_png_href) ();
+              link ~rel: [`Icon] ~a: [a_mime_type "image/png"; a_sizes (Some [(16, 16)])] ~href: (Lazy.force favicon_16x16_png_href) ();
+              link ~rel: [`Manifest] ~href: (Lazy.force site_webmanifest_href) ();
+              (* Ahrefs ownership proof and analytics *)
+              meta ~a: [a_name "ahrefs-site-verification"; a_content "4c418f04303adf3925d1c6bdef51b71cab26cd179ab478ecd1996cf71ce52ba4"] ();
+              script ~a: [a_src "https://analytics.ahrefs.com/analytics.js"; a_user_data "key" "HmcihempNgdCWYAHGvIYsg"; a_async ()] (txt "");
+              (* Dancelor *)
+              script ~a: [a_script_type `Javascript; a_src (Lazy.force client_js_href)] (txt "");
             ]
-        )
-  in
-  let headers = Cohttp.Header.of_list [("Content-Type", "text/html")] in
-  Cohttp_lwt_unix.Server.respond_string ~headers ~status: `OK ~body: index ()
+          )
+          (
+            body
+              ~a: [a_class ["placeholder-glow"]]
+              [
+                script ~a: [a_src (Lazy.force bootstrap_bundle_min_js_href)] (txt "");
+              ]
+          )
+    in
+    let headers = Cohttp.Header.of_list [("Content-Type", "text/html")] in
+    Cohttp_lwt_unix.Server.respond_string ~headers ~status: `OK ~body: index ()
 
 let static_pages = List.map Uri.of_string ["/"; "/explore"]
 
@@ -91,26 +109,25 @@ let serve_robots_txt () =
 
 let serve env path =
   Log.debug (fun m -> m "Looking to serve %S" path);
-  let full_path = Filename.concat (Config.get ()).share path in
-  if Sys.file_exists full_path && not (Sys.is_directory full_path) then
+  match serve_static_file path with
+  | Some serve_static_file ->
     (
       Log.debug (fun m -> m "Serving static file: <share>/%s" @@ String.ltrim ~chars: ['/'] path);
-      (* Keep static files in cache for 30 days. *)
-      let headers = Cohttp.Header.init_with "Cache-Control" "max-age=2592000" in
-      Cohttp_lwt_unix.Server.respond_file ~headers ~fname: full_path ()
+      serve_static_file ()
     )
-  else if path = "/sitemap.xml" then
-    (
-      Log.debug (fun m -> m "Generating and serving sitemap.xml");
-      serve_sitemap env
-    )
-  else if path = "/robots.txt" then
-    (
-      Log.debug (fun m -> m "Serving robots.txt");
-      serve_robots_txt ()
-    )
-  else
-    (
-      Log.debug (fun m -> m "Serving main file.");
-      serve_index path
-    )
+  | None ->
+    if path = "/sitemap.xml" then
+      (
+        Log.debug (fun m -> m "Generating and serving sitemap.xml");
+        serve_sitemap env
+      )
+    else if path = "/robots.txt" then
+      (
+        Log.debug (fun m -> m "Serving robots.txt");
+        serve_robots_txt ()
+      )
+    else
+      (
+        Log.debug (fun m -> m "Serving main file.");
+        serve_index path
+      )
