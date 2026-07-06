e0c35ba2f3d4f2085ce4c69098c05d41c19ff837
Niols <niols@niols.fr>
2026-07-06 10:01:25 +0200

    Monitor a better breakdown of HTTP requests

--- src/server/metrics.ml
+++ src/server/metrics.ml
@@ -8,6 +8,22 @@ let http_requests_total =
 
 let increment_http_requests_total () = Prometheus.Counter.inc_one http_requests_total
 
+let http_requests_count =
+  let family =
+    Prometheus.Counter.v_labels
+      ~namespace
+      ~help: "Number of HTTP requests"
+      "http_requests_count"
+      ~label_names: ["kind"; "file"]
+  in
+  fun ~kind ~file ->
+    Prometheus.Counter.labels family [kind; file]
+
+let increment_http_requests_count = function
+  | `API -> Prometheus.Counter.inc_one (http_requests_count ~kind: "API" ~file: "")
+  | `Main_file -> Prometheus.Counter.inc_one (http_requests_count ~kind: "Main file" ~file: "")
+  | `Static file -> Prometheus.Counter.inc_one (http_requests_count ~kind: "Static" ~file)
+
 let open_file_descriptors =
   Prometheus.Gauge.v
     ~namespace
