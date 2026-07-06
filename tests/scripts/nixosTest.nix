0126c2d68777a2f7b5de6683f2a1e514cdedb9e5
Niols <niols@niols.fr>
2026-07-06 02:35:02 +0200

    Fix systemd units ordering in integration test

--- tests/scripts/nixosTest.nix
+++ tests/scripts/nixosTest.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  inherit (lib)
+    mkForce
+    ;
+
+in
 {
   nodes.machine =
     { pkgs, ... }:
@@ -19,12 +25,36 @@
       };
 
       environment.systemPackages = [ pkgs.postgresql ];
+
+      ## Add a small systemd unit that injects the test database into
+      ## PostgreSQL. It must run after PostgreSQL has booted up but
+      ## before Dancelor.
+      systemd.services.inject-test-database = {
+        after = [
+          "postgresql.service"
+          "postgresql-setup.service"
+        ];
+        requires = [
+          "postgresql.service"
+          "postgresql-setup.service"
+        ];
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          User = "dancelor";
+          ExecStart = "${pkgs.postgresql}/bin/psql --dbname=dancelor --file=${../database.sql}";
+        };
+      };
+      systemd.services.dancelor = {
+        after = [ "inject-test-database.service" ];
+        requires = [ "inject-test-database.service" ];
+        serviceConfig.Restart = mkForce "no"; # only try starting once
+      };
     };
 
   testScript = ''
     machine.start()
     machine.wait_for_unit("dancelor.service")
-    machine.succeed("sudo -u dancelor psql --dbname=dancelor --file=${../database.sql}")
     machine.succeed("cd ${../..} && pytest --numprocesses auto --verbose >&2")
   '';
 }
